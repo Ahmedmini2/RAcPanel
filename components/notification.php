a2adb8e4dd9a9e91ccde98d7f49a7231297f9e60
Ahmedmini2 <liltemes@gmail.com>
2023-09-09 02:13:37 +0400

    يييي

--- components/notification.php
+++ components/notification.php
@@ -2,8 +2,8 @@
 
 if(isset($_SESSION['notification'])){
     echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
-    <span class="alert-icon"><i class="ni ni-like-2"></i></span>
-    <span class="alert-text"><strong>Success!</strong> '.$_SESSION['notification'].'</span>
+    
+    <span class="alert-text"><strong></strong> '.$_SESSION['notification'].'</span>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
