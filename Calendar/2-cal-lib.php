7136156e5dad652c0173cff88eeecaeaed41e65d
Ahmedmini2 <liltemes@gmail.com>
2023-09-09 12:45:52 +0400

    C

--- Calendar/2-cal-lib.php
+++ Calendar/2-cal-lib.php
@@ -6,8 +6,8 @@ class Calendar {
   public $error = "";
   function __construct () {
     $this->pdo = new PDO(
-      "mysql:host=localhost;dbname=calender;charset=".DB_CHARSET,
-      'root', '', [
+      "mysql:host=localhost;dbname=ruknam5_app;charset=".DB_CHARSET,
+      'ruknam5_root', 'Roek9933@', [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
