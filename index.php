7ec6aa8d7f2e36f70bdaf65767c3028df246681d
Ahmedmini2 <liltemes@gmail.com>
2023-09-11 02:21:45 +0400

    sss

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ include ('cookies/session.php');
         <div class="collapse navbar-collapse px-0 w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link  " href="index.php">
+              <a class="nav-link  active" href="index.php">
                 <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center">
                   <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <title>shop </title>
@@ -59,7 +59,7 @@ include ('cookies/session.php');
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" href="../Accounts/accounts.php">
+              <a class="nav-link " href="../Accounts/accounts.php">
                 <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center">
                   <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <title>office</title>
