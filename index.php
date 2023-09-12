fcfc4bbdd1a8a2e74cf303528a736007186b171e
Ahmedmini2 <liltemes@gmail.com>
2023-09-12 23:19:36 +0400

    Add Notifications

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include ('cookies/session.php');
+
 ?>
 
 <html lang="ar" dir="rtl">
@@ -22,6 +23,48 @@ include ('cookies/session.php');
   <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <script>
+  // Function to fetch notifications from the server
+  function fetchNotifications() {
+    // Make an AJAX request to the server to fetch notifications
+    $.ajax({
+      url: 'fetch_notifications.php', // Replace with the actual URL
+      method: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        // Process the notifications data and update the notification UI
+        updateNotificationUI(data);
+      },
+      error: function (error) {
+        console.error('Error fetching notifications:', error);
+      },
+    });
+  }
+
+  // Function to update the notification UI with new data
+  function updateNotificationUI(data) {
+    // Clear the existing notifications
+    $('#notifications-container').empty();
+
+    // Loop through the received notifications and add them to the UI
+    data.forEach(function (notification) {
+      const notificationItem = $('<li>').addClass('mb-2');
+      const notificationLink = $('<a>').addClass('dropdown-item border-radius-md').attr('href', 'javascript:;');
+      // Customize the notificationItem and notificationLink based on your data structure
+      // You can use notification.title, notification.message, notification.timestamp, etc.
+      // Example:
+      // notificationLink.html('<h6>' + notification.title + '</h6><p>' + notification.message + '</p>');
+      notificationItem.append(notificationLink);
+      $('#notifications-container').append(notificationItem);
+    });
+  }
+
+  // Fetch notifications initially
+  fetchNotifications();
+
+  // Poll for new notifications every 5 minutes (adjust the interval as needed)
+  setInterval(fetchNotifications, 300000); // 5 minutes = 300,000 milliseconds
+</script>
 </head>
 
 <body class="g-sidenav-show rtl bg-gray-100">
@@ -234,93 +277,17 @@ include ('cookies/session.php');
                 <span class="d-sm-inline d-none"> تسجيل الخروج</span>
               </a>
             </li>
-            <li class="nav-item d-xl-none pe-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li>
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0">
-                <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
-              </a>
-            </li>
+            <!-- Notifications -->
             <li class="nav-item dropdown ps-2 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fa fa-bell cursor-pointer"></i>
-              </a>
-              <ul class="dropdown-menu  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
-                <li class="mb-2">
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="my-auto">
-                        <img src="assets/img/team-2.jpg" class="avatar avatar-sm  ms-3 ">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New message</span> from Laur
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          13 minutes ago
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li class="mb-2">
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="my-auto">
-                        <img src="assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm bg-gradient-dark  ms-3 ">
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New album</span> by Travis Scott
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          1 day
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item border-radius-md" href="javascript:;">
-                    <div class="d-flex py-1">
-                      <div class="avatar avatar-sm bg-gradient-secondary  ms-3  my-auto">
-                        <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                          <title>credit-card</title>
-                          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <g transform="translate(-2169.000000, -745.000000)" fill="#FFFFFF" fill-rule="nonzero">
-                              <g transform="translate(1716.000000, 291.000000)">
-                                <g transform="translate(453.000000, 454.000000)">
-                                  <path class="color-background" d="M43,10.7482083 L43,3.58333333 C43,1.60354167 41.3964583,0 39.4166667,0 L3.58333333,0 C1.60354167,0 0,1.60354167 0,3.58333333 L0,10.7482083 L43,10.7482083 Z" opacity="0.593633743"></path>
-                                  <path class="color-background" d="M0,16.125 L0,32.25 C0,34.2297917 1.60354167,35.8333333 3.58333333,35.8333333 L39.4166667,35.8333333 C41.3964583,35.8333333 43,34.2297917 43,32.25 L43,16.125 L0,16.125 Z M19.7083333,26.875 L7.16666667,26.875 L7.16666667,23.2916667 L19.7083333,23.2916667 L19.7083333,26.875 Z M35.8333333,26.875 L28.6666667,26.875 L28.6666667,23.2916667 L35.8333333,23.2916667 L35.8333333,26.875 Z"></path>
-                                </g>
-                              </g>
-                            </g>
-                          </g>
-                        </svg>
-                      </div>
-                      <div class="d-flex flex-column justify-content-center">
-                        <h6 class="text-sm font-weight-normal mb-1">
-                          Payment successfully completed
-                        </h6>
-                        <p class="text-xs text-secondary mb-0">
-                          <i class="fa fa-clock me-1"></i>
-                          2 days
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              </ul>
+                <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-bell cursor-pointer"></i>
+                </a>
+                <ul class="dropdown-menu  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton" id="notifications-container">
+                    <!-- Notifications will be dynamically added here -->
+                </ul>
             </li>
+              <!-- End of Notifications -->
+
           </ul>
         </div>
       </div>
---
+++ scripts/notifications/fetch_notification.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+$email_address= !empty($_SESSION['email'])?$_SESSION['email']:'';
+$id= !empty($_SESSION['id'])?$_SESSION['id']:'';
+$role = !empty($_SESSION['role'])?$_SESSION['role']:'';
+$position = !empty($_SESSION['position'])?$_SESSION['position']:'';
+if($email_address == '')
+{
+  header("location:../../Auth/sign-in.php");
+}
+include '../../db/connection.php';
+$query = "SELECT * FROM notifications ORDER BY created_at DESC LIMIT 10";
+$result = $conn->query($query);
+
+$notifications = array();
+
+if ($result) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $notifications[] = array(
+            'title' => $row['name'],
+            'message' => $row['description'],
+            'timestamp' => $row['created_at']
+        );
+    }
+}
+
+// Return the notifications as JSON
+header('Content-Type: application/json');
+echo json_encode($notifications);
+
+?>
