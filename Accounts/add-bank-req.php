65cc4bfc442f0036ac8c56ca19524ddf09dbcad6
mohammed jamal <yamni.zero@gmail.com>
2023-09-13 13:55:17 +0200

    btn color

--- Accounts/add-bank-req.php
+++ Accounts/add-bank-req.php
@@ -418,7 +418,6 @@ if (!empty($_GET['edit'])) {
       <div class="row">
         <div class="block block-themed">
           <div class="block-header bg-warning col-md-3 col-sm-6 col-xs-6 rounded">
-
             <?php require_once('../components/notification.php'); ?>
           </div>
           <div class="block-header bg-warning col-md-2 col-sm-6 col-xs-6  rounded">
