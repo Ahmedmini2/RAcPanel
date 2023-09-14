1c8311993f977b2a21bebc97c7b11dd55cf618bf
mohammed jamal <yamni.zero@gmail.com>
2023-09-15 00:43:24 +0200

    s

--- Beneficiary/add-beneficiary-bank.php
+++ Beneficiary/add-beneficiary-bank.php
@@ -400,7 +400,7 @@ if(!empty($_GET['edit'])){
                                     
             <?php require_once('../components/notification.php'); ?>
           </div>
-                                <div class="block-header bg-warning  col-md-2 col-sm-6 col-xs-6  rounded">
+                                <div class="block-header bg-warning  col-md-2 col-sm-6 col-xs-6  rounded-pill">
                                     
                                     <h5 class="block-title py-2 px-4 ">إضافة حساب مستفيد جديد</h5>
                                 </div>
