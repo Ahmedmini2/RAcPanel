b440ca7bd137e20918cc214fa29bf214c2487ba5
mohammed jamal <yamni.zero@gmail.com>
2023-09-18 00:46:28 +0200

    +

--- Projects/index.php
+++ Projects/index.php
@@ -160,7 +160,7 @@ $_SESSION['sidebar']="Projects";
               <h6 class="mb-1">المشاريع</h6>
             </div>
             <button type="button" id="btn3" class="printing printing2 btn bg-gradient-dark rounded-pill col-md-2 col-sm-6 col-xs-6 " data-bs-toggle="modal" data-bs-target="#exampleModal2">
-              + أضافة مشروع
+               أضافة مشروع +
       </button>
             <div class="card-body p-3  ">
               <div class="row">
