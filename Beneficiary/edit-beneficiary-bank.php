0d7cff1bce76264555b860bc68b84fcf55bc53b5
mohammed jamal <yamni.zero@gmail.com>
2023-09-17 14:43:07 +0200

    add user

--- Beneficiary/edit-beneficiary-bank.php
+++ Beneficiary/edit-beneficiary-bank.php
@@ -379,13 +379,13 @@ include ('../cookies/session2.php');
     <div class="container-fluid py-4">
         <div class="row">
         <div class="block block-themed">
-          <div class="block-header bg-warning  col-md-3 col-sm-6 col-xs-6  rounded">
+          <div class="block-header bg-gradient-dark  col-md-3 col-sm-6 col-xs-6  rounded-pill">
                                     
             <?php require_once('../components/notification.php'); ?>
           </div>
-                                <div class="block-header bg-warning  col-md-2 col-sm-6 col-xs-6  rounded">
+                                <div class="block-header bg-gradient-dark  col-md-2 col-sm-6 col-xs-6  rounded-pill">
                                     
-                                    <h5 class="block-title py-2 px-4">إضافة حساب مستفيد جديد</h5>
+                                    <h5 class="block-title text-white py-2 px-4">إضافة حساب مستفيد جديد</h5>
                                 </div>
                                 <form id="<?php echo $idAttr; ?>" action="" method="post">
                                 <div class="row">
