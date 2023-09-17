eab84a8574cd8339ab4bba9854c9a13bd09422ce
mohammed jamal <yamni.zero@gmail.com>
2023-09-18 00:15:32 +0200

    py-3

--- Projects/index.php
+++ Projects/index.php
@@ -160,10 +160,10 @@ $_SESSION['sidebar']="Projects";
               <h6 class="mb-1">المشاريع</h6>
               <p class="text-sm">إدارة اخر المشاريع</p>
             </div>
-            <div class="card-body p-3 py-5 ">
+            <div class="card-body p-3  ">
               <div class="row">
                 <div class="col-xl-3 col-md-6 mb-xl-0 mb-4  ">
-                  <div class="card card-blog card-plain">
+                  <div class="card card-blog card-plain py-3">
                     <div class="position-relative">
                       <a class="d-block shadow-xl border-radius-xl">
                         <img src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
@@ -187,7 +187,7 @@ $_SESSION['sidebar']="Projects";
                   </div>
                 </div>
                 <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 ">
-                  <div class="card card-blog card-plain">
+                  <div class="card card-blog card-plain py-3">
                     <div class="position-relative">
                       <a class="d-block shadow-xl border-radius-xl">
                         <img src="../assets/img/home-decor-2.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-lg">
@@ -211,7 +211,7 @@ $_SESSION['sidebar']="Projects";
                   </div>
                 </div>
                 <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 ">
-                  <div class="card card-blog card-plain">
+                  <div class="card card-blog card-plain py-3">
                     <div class="position-relative">
                       <a class="d-block shadow-xl border-radius-xl">
                         <img src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
@@ -235,7 +235,7 @@ $_SESSION['sidebar']="Projects";
                   </div>
                 </div>
                 <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 ">
-                  <div class="card card-blog card-plain">
+                  <div class="card card-blog card-plain py-3">
                     <div class="position-relative">
                       <a class="d-block shadow-xl border-radius-xl">
                         <img src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
@@ -259,7 +259,7 @@ $_SESSION['sidebar']="Projects";
                   </div>
                 </div>
                 <div class="col-xl-3 col-md-6 mb-xl-0 mb-4  ">
-                  <div class="card card-blog card-plain">
+                  <div class="card card-blog card-plain py-3">
                     <div class="position-relative">
                       <a class="d-block shadow-xl border-radius-xl">
                         <img src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
