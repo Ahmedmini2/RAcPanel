c02fa926fe257b76c528e1977d99a2aedd25e98b
Ahmedmini2 <liltemes@gmail.com>
2023-09-17 23:10:18 +0400

    dd

--- components/sidebar.php
+++ components/sidebar.php
@@ -141,7 +141,7 @@ $sidebar = $_SESSION['sidebar'];
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link <?php if($sidebar == "Projects") echo 'active'; ?>" href="Projects/index.php">
+              <a class="nav-link <?php if($sidebar == "Projects") echo 'active'; ?>" href="../Projects/index.php">
                 <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center">
                   <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                     <title>document</title>
