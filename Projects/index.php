aab3cf699263c7fe1c6441db0e7dad3fef3dbc09
mohammed jamal <yamni.zero@gmail.com>
2023-09-18 13:26:09 +0200

    details

--- Projects/index.php
+++ Projects/index.php
@@ -179,6 +179,10 @@ $_SESSION['sidebar'] = "Projects";
                   <label for="formGroupExampleInput" class="form-label">اسم المشروع</label>
                   <input type="text" class="form-control" id="formGroupExampleInput" placeholder="ادخل اسم المشروع">
                 </div>
+                <div class="mb-3">
+                  <label for="formGroupExampleInput" class="form-label">تفاصيل المشروع</label>
+                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="ادخل تفاصيل المشروع">
+                </div>
                 <form method="post" action="../scripts/update-status/update.php?bank_req=<?= $id ?>" enctype="multipart/form-data">
                   <input type="file" name="fileToUpload" id="fileToUpload" class="form-control">
                   <input type="submit" value="Upload Image" name="upload" class="btn bg-gradient-dark m-4 rounded-pill">
