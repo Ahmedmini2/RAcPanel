f3d10d4d068423558915cabc57a577ba3b3c71dc
mohammed jamal <yamni.zero@gmail.com>
2023-09-18 15:32:49 +0200

    ss

--- Projects/add-projects.php
+++ Projects/add-projects.php
@@ -103,7 +103,7 @@ if(!empty($_GET['edit'])){
       <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 ">
-            <li class="breadcrumb-item text-sm ps-2"><a class="opacity-5 text-dark" href="javascript:;"> أضافة حساب مستفيد جديد</a></li>
+            <li class="breadcrumb-item text-sm ps-2"><a class="opacity-5 text-dark" href="javascript:;"> أضافة مشروع جديد</a></li>
             
           </ol>
 
