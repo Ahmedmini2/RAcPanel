062449360cf6c1d40dca5cbfbea89dec78910a92
Ahmedmini2 <liltemes@gmail.com>
2023-09-21 19:28:39 +0400

    dd

--- Projects/script.js
+++ Projects/script.js
@@ -2,30 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const addProductButton = document.querySelector(".add_product");
     const productDetails = document.querySelector("#product_details");
 
-     // Function to calculate and update the total
-     function updateTotal(item) {
+    // Function to calculate and update the total
+    function updateTotal(item) {
         const quantity = parseFloat(item.querySelector("[name='quantity[]']").value);
         const kh_price = parseFloat(item.querySelector("[name='kh_price']").value);
         const kh_per = parseFloat(item.querySelector("[name='kh_per']").value);
-        const totalField = item.querySelector("[name='kh_tot']");
-        const total = (kh_price * kh_per * quantity).toFixed(2); // Calculate total
-  
-        totalField.value = total; // Update the total field
-      }
-  
-      // Add event listeners to the input fields
-      productDetails.addEventListener("input", function (e) {
-        if (
-          e.target.name === "quantity[]" ||
-          e.target.name === "kh_price" ||
-          e.target.name === "kh_per"
-        ) {
-          const item = e.target.closest(".item");
-          if (item) {
-            updateTotal(item);
-          }
+        const totalField = item.querySelector(".kh_tot");
+
+        if (!isNaN(quantity) && !isNaN(kh_price) && !isNaN(kh_per)) {
+            const total = ( kh_per * quantity * kh_price).toFixed(2); // Calculate total
+            totalField.value = total; // Update the total field
+        } else {
+            totalField.value = ""; // Clear the total if any input is not a number
         }
-      });
+    }
+
+    // Add event listeners to the input fields
+    productDetails.addEventListener("input", function (e) {
+        if (
+            e.target.name === "quantity[]" ||
+            e.target.name === "kh_price" ||
+            e.target.name === "kh_per"
+        ) {
+            const item = e.target.closest(".item");
+            if (item) {
+                updateTotal(item);
+            }
+        }
+    });
 
     addProductButton.addEventListener("click", function () {
         const productClone = document.querySelector(".product").cloneNode(true);
