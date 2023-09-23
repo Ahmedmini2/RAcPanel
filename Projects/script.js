5df7a3cc2269ec7e13af6a778931f0e01ca0fa56
Ahmedmini2 <liltemes@gmail.com>
2023-09-24 01:49:50 +0400

    fffff

--- Projects/script.js
+++ Projects/script.js
@@ -8,30 +8,34 @@ document.addEventListener("DOMContentLoaded", function () {
         productDetails.appendChild(productClone);
     });
 
-    let counter = 1;
-    let cloning = false;
-    
-    productDetails.addEventListener("click", function (e) {
-        if (e.target.classList.contains("add_iron") && !cloning) {
-            cloning = true;
-            const itemDetails = e.target.parentElement.querySelector(".iron_details");
-            const itemClone = document.querySelector(".iron").cloneNode(true);
-            
-            // Generate unique IDs for the cloned elements
-            itemClone.querySelectorAll("[id]").forEach((element) => {
-                element.id += counter;
-            });
-            
-            // Update the name attributes if needed
-            itemClone.querySelectorAll("[name]").forEach((element) => {
-                // Modify the name attribute based on your naming convention
-                element.name += "_" + counter;
-            });
-            
-            itemDetails.appendChild(itemClone);
-            counter++;
-            cloning = false;
-        }
+    // Assuming you have a counter variable to generate unique IDs
+        let counter = 1;
+        let isCloning = false;
+
+        productDetails.addEventListener("click", function (e) {
+            if (e.target.classList.contains("add_iron") && !isCloning) {
+                isCloning = true;
+                setTimeout(function () {
+                    const itemDetails = e.target.parentElement.querySelector(".iron_details");
+                    const itemClone = document.querySelector(".iron").cloneNode(true);
+                    
+                    // Generate unique IDs for the cloned elements
+                    itemClone.querySelectorAll("[id]").forEach((element) => {
+                        element.id += counter;
+                    });
+                    
+                    // Update the name attributes if needed
+                    itemClone.querySelectorAll("[name]").forEach((element) => {
+                        // Modify the name attribute based on your naming convention
+                        element.name += "_" + counter;
+                    });
+                    
+                    itemDetails.appendChild(itemClone);
+                    counter++;
+                    isCloning = false;
+                }, 1000); // Adjust the delay time (in milliseconds) as needed
+            }
+
     
 
         if (e.target.classList.contains("add_accessory")) {
