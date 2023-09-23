1426ddf97c6017b1e07a783832dc93258ca1b356
mohammed jamal <yamni.zero@gmail.com>
2023-09-23 15:31:59 +0200

    hr css

--- Projects/add-projects.php
+++ Projects/add-projects.php
@@ -91,7 +91,33 @@ if (!empty($_GET['edit'])) {
   <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <style>
+/* Red border */
+hr.new1 {
+  border-top: 1px solid red;
+}
 
+/* Dashed red border */
+hr.new2 {
+  border-top: 3px dashed black;
+}
+
+/* Dotted red border */
+hr.new3 {
+  border-top: 1px dotted red;
+}
+
+/* Thick red border */
+hr.new4 {
+  border: 1px solid red;
+}
+
+/* Large rounded green border */
+hr.new5 {
+  border: 10px solid green;
+  border-radius: 5px;
+}
+</style>
 </head>
 
 <body class="g-sidenav-show rtl bg-gray-100">
@@ -376,7 +402,7 @@ if (!empty($_GET['edit'])) {
                         </div>
                       </div>
                     </div>
-                    <hr style="border-bottom: 2px dashed #344767">
+                    <hr class="new2">
                     <script>
                       $(document).ready(function() {
                         $("#iron").change(function() {
