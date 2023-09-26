ae3b22d1b7faf4a227d2669e5fcf93fa79f375fd
Ahmedmini2 <liltemes@gmail.com>
2023-09-27 01:46:07 +0400

    raraar

--- Projects/script.js
+++ Projects/script.js
@@ -2,14 +2,10 @@ var i=$('#rowcount').val();
 var a=$('#rowcount_ac').val();
 var b=$('#rowcount_band').val();
 document.addEventListener("DOMContentLoaded", function () {
-    const addProductButton = document.querySelector(".add_product");
+    
     const productDetails = document.querySelector("#product_details");
 
-   // Function to calculate and update the total for each item
-    addProductButton.addEventListener("click", function () {
-        const productClone = document.querySelector(".product").cloneNode(true);
-        productDetails.appendChild(productClone);
-    });
+   
 
     productDetails.addEventListener("click", function (e) {
         if (e.target.classList.contains("add_iron")) {
