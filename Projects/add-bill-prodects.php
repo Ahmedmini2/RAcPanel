53755d2168c6e5baf720007dc7f0cbed8f0d58be
mohammed jamal <yamni.zero@gmail.com>
2023-09-27 01:59:44 +0200

    mt-5

--- Projects/add-bill-prodects.php
+++ Projects/add-bill-prodects.php
@@ -335,7 +335,7 @@ hr.new5 {
               <div class="row">
                 <div class="col text-center">
                   <div class="form-group">
-                    <button type="submit" name="submit" class=" col-md-6 col-sm-6 mt-10 btn btn-secondary rounded-pill"> حفظ </button>
+                    <button type="submit" name="submit" class=" col-md-6 col-sm-6 mt-5 btn btn-secondary rounded-pill"> حفظ </button>
                   </div>
                 </div>
               </div>
