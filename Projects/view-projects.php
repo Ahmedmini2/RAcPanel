d097d79c9f5f886fffd73fde4b9a6188031f1308
mohammed jamal <yamni.zero@gmail.com>
2023-09-27 22:39:23 +0200

    table

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -302,7 +302,7 @@ $_SESSION['sidebar'] = "Projects";
                                         </div>
                                         <div class="card-body pt-0 p-3 text-center">
                                             <h6 class="text-center mb-0">حاله المشروع</h6>
-            
+
                                             <td class="align-middle">
                                                 <div class="progress-wrapper w-75 mx-auto">
                                                     <div class="progress-info">
@@ -325,6 +325,85 @@ $_SESSION['sidebar'] = "Projects";
                         </div>
 
                     </div>
+                    <!--Table-->
+                    <table class="table table-hover table-fixed">
+
+                        <!--Table head-->
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Surname</th>
+                                <th>Country</th>
+                                <th>City</th>
+                                <th>Position</th>
+                                <th>Age</th>
+                            </tr>
+                        </thead>
+                        <!--Table head-->
+
+                        <!--Table body-->
+                        <tbody>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Jerry</td>
+                                <td>Horwitz</td>
+                                <td>Italy</td>
+                                <td>Bari</td>
+                                <td>Editor-in-chief</td>
+                                <td>41</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">5</th>
+                                <td>Janis</td>
+                                <td>Joplin</td>
+                                <td>Poland</td>
+                                <td>Warsaw</td>
+                                <td>Video Maker</td>
+                                <td>39</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">6</th>
+                                <td>Gary</td>
+                                <td>Winogrand</td>
+                                <td>Germany</td>
+                                <td>Berlin</td>
+                                <td>Photographer</td>
+                                <td>37</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">7</th>
+                                <td>Angie</td>
+                                <td>Smith</td>
+                                <td>USA</td>
+                                <td>San Francisco</td>
+                                <td>Teacher</td>
+                                <td>52</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">8</th>
+                                <td>John</td>
+                                <td>Mattis</td>
+                                <td>France</td>
+                                <td>Paris</td>
+                                <td>Actor</td>
+                                <td>28</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">9</th>
+                                <td>Otto</td>
+                                <td>Morris</td>
+                                <td>Germany</td>
+                                <td>Munich</td>
+                                <td>Singer</td>
+                                <td>35</td>
+                            </tr>
+                        </tbody>
+                        <!--Table body-->
+
+                    </table>
+                    <!--Table-->
+
                     <footer class="footer pt-3  ">
                         <div class="container-fluid">
                             <div class="row align-items-center justify-content-lg-between">
