0e4b8713748f536e1cc80272e6af34e872e7d7f1
mohammed jamal <yamni.zero@gmail.com>
2023-09-27 23:21:03 +0200

    ss

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -332,7 +332,7 @@ $_SESSION['sidebar'] = "Projects";
                                 <div class="card-header pb-0">
                                     <h6>الاصناف</h6>
                                 </div>
-                                <div class="card-body px-0 pt-0 pb-2">
+                                <div class="card-body px-0 pt-0 pb-2 mx-3">
                                     <div class="table-responsive p-0">
                                         <table class="table table-hover table-fixed">
 
