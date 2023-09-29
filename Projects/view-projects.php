dd7fefad38cf7bcdd72dddf61a1a9db11246a0cf
mohammed jamal <yamni.zero@gmail.com>
2023-09-29 15:28:25 +0200

    s

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -151,17 +151,18 @@ $_SESSION['sidebar'] = "Projects";
             </div>
         </nav>
         <!-- End Navbar -->
-        <div class="row">
 
 
-            <div class="container-fluid py-4">
+
+        <div class="container-fluid py-4">
+            <div class="row">
                 <div class="block-header bg-gradient-dark  col-md-3 col-sm-6 col-xs-6  rounded-pill">
 
                     <?php require_once('../components/notification.php'); ?>
                 </div>
                 <!--********* -->
                 <div class="col-12 mt-4">
-                    <div class="card mb-4 p-3">
+                    <div class="card mx-4 p-3">
                         <div class="card-header">
                             <h3 class="mb-1">عرض مشروع</h3>
                         </div>
@@ -537,44 +538,46 @@ $_SESSION['sidebar'] = "Projects";
 
 
                     </div>
-                    <button type="button" id="btnD" class=" btn bg-gradient-dark rounded-pill col-md-2 col-sm-6 col-xs-6 mt-4 ">
-                        حذف المشروع 
-                    </button>
+
                 </div>
 
             </div>
+            <button type="button" id="btnD" class=" btn bg-gradient-dark rounded-pill col-md-2 col-sm-6 col-xs-6 mx-4 ">
+            حذف المشروع
+        </button>
+        </div>
 
+        
+        <footer class="footer pt-3  ">
+            <div class="container-fluid">
+                <div class="row align-items-center justify-content-lg-between">
+                    <div class="col-lg-6 mb-lg-0 mb-4">
+                        <div class="copyright text-center text-sm text-muted text-lg-end">
+                            © <script>
+                                document.write(new Date().getFullYear())
+                            </script>,
+                            made with <i class="fa fa-heart"></i> by
+                            <a href="" class="font-weight-bold" target="_blank">Rukn Amial</a>
 
-            <footer class="footer pt-3  ">
-                <div class="container-fluid">
-                    <div class="row align-items-center justify-content-lg-between">
-                        <div class="col-lg-6 mb-lg-0 mb-4">
-                            <div class="copyright text-center text-sm text-muted text-lg-end">
-                                © <script>
-                                    document.write(new Date().getFullYear())
-                                </script>,
-                                made with <i class="fa fa-heart"></i> by
-                                <a href="" class="font-weight-bold" target="_blank">Rukn Amial</a>
-
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                                <li class="nav-item">
-                                    <a href="https://ruknamial.com" class="nav-link text-muted" target="_blank">Rukn Amial</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://files.ruknamial.com" class="nav-link text-muted" target="_blank">Files</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://ruknamial.com/blogs" class="nav-link text-muted" target="_blank">Blog</a>
-                                </li>
-
-                            </ul>
                         </div>
                     </div>
-            </footer>
-        </div>
+                    <div class="col-lg-6">
+                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                            <li class="nav-item">
+                                <a href="https://ruknamial.com" class="nav-link text-muted" target="_blank">Rukn Amial</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://files.ruknamial.com" class="nav-link text-muted" target="_blank">Files</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://ruknamial.com/blogs" class="nav-link text-muted" target="_blank">Blog</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+        </footer>
+
 
     </main>
 
