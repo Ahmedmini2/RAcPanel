b2d9122a4221815153ff4c1ff10784da9e3dfdac
mohammed jamal <yamni.zero@gmail.com>
2023-10-01 13:30:12 +0200

    as

--- components/sidebar.php
+++ components/sidebar.php
@@ -58,9 +58,9 @@ $sidebar = $_SESSION['sidebar'];
       <?php if ($position == 'Admin') { ?><li class="nav-item">
 
         <li class="nav-item">
-          <a class="nav-link <?php if ($sidebar == "Users") echo 'active'; ?>" href="../Users/users.php">
+          <div class="dropdown">
 
-            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center " type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center ">
               <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>credit-card</title>
                 <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -75,13 +75,18 @@ $sidebar = $_SESSION['sidebar'];
                 </g>
               </svg>
             </div>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+              <button class="dropdown-item" type="button">Action</button>
+              <button class="dropdown-item" type="button">Another action</button>
+              <button class="dropdown-item" type="button">Something else here</button>
+            </div>
+          </div>
+          <a class="nav-link <?php if ($sidebar == "Users") echo 'active'; ?>" href="../Users/users.php">
+
+
             <span class="nav-link-text me-1">المستخدمين</span>
           </a>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <button class="dropdown-item" type="button">Action</button>
-            <button class="dropdown-item" type="button">Another action</button>
-            <button class="dropdown-item" type="button">Something else here</button>
-          </div>
+
         </li>
       <?php } ?>
       <li class="nav-item">
