baf36ddb830617a8e2c2886b94d918f4768a292b
Ahmedmini2 <liltemes@gmail.com>
2023-10-01 12:55:53 +0400

    dwada

--- Projects/add-projects.php
+++ Projects/add-projects.php
@@ -659,8 +659,10 @@ if(isset($_POST['add-project'])){
                         var kg = (parseFloat($("#iron_quantity_"+z).val()) * parseFloat($("#iron_long_"+z).val() || '0') * iron)
                         var tn = kg / 1000;
                         var total = tn * parseFloat($("#iron_price_"+z).val())
-                        $("#iron_tn_"+z).val(tn).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-                        $("#iron_tot_"+z).val(total).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+                        tn = tn.toLocaleString("en-US");
+                        total = total.toLocaleString("en-US");
+                        $("#iron_tn_"+z).val(tn);
+                        $("#iron_tot_"+z).val(total);
                         total_iron += total;
                         }
                         $("#total-iron").val(total_iron);
