35638f7e0a1744b69a13378b70a30930c30a2ea5
Ahmedmini2 <liltemes@gmail.com>
2023-10-01 10:46:11 +0400

    daw

--- scripts/accounts/delete.php
+++ scripts/accounts/delete.php
@@ -30,6 +30,7 @@ include '../../db/connection.php';
                                 }
                             }else{
                                 $_SESSION['notification'] = "كلمة مرور خاطئة";
+                                header('location:../../Accounts/accounts.php');
                             }
                         
                         }
