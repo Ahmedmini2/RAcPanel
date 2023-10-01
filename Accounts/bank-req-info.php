b94ae1cd346b7ef1cea0924f612df4e71a486715
mohammed jamal <yamni.zero@gmail.com>
2023-10-01 09:37:51 +0200

    font-size

--- Accounts/bank-req-info.php
+++ Accounts/bank-req-info.php
@@ -73,7 +73,7 @@ if (isset($_GET['bank_req'])) {
     .row1 {
       padding-top: 40px !important;
       position: relative;
-
+      font-size :small;
       text-align: right !important;
     }
 
@@ -81,6 +81,8 @@ if (isset($_GET['bank_req'])) {
       position: relative;
       left: 5%;
       text-align: left !important;
+      padding-top: 40px !important;
+      font-size :small;
     }
 
     .invoice-box table {
@@ -92,6 +94,7 @@ if (isset($_GET['bank_req'])) {
     .invoice-box table td {
       padding: 5px;
       vertical-align: top;
+      font-size :small;
     }
 
     .invoice-box table tr td:nth-child(2) {
