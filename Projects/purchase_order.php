4c2cdf05b64f5dc494b80162550c22c29b5f23d8
mohammed jamal <yamni.zero@gmail.com>
2023-10-02 21:46:14 +0200

    xa

--- Projects/purchase_order.php
+++ Projects/purchase_order.php
@@ -475,7 +475,7 @@ if (isset($_GET['bank_req'])) {
                         </div>
                     </div>
                     <div class=" col-sm-6 col-lg-4 mx-7">
-                        <div class="container">
+                        <div class="card">
                             <div class="card-header text-center text-white" style="background:#3A416F">
                                 VENDOR
                             </div>
