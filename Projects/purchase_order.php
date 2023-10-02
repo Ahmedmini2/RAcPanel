a0eda60f7751be98c8296f16298bb6072386c472
mohammed jamal <yamni.zero@gmail.com>
2023-10-02 20:27:36 +0200

    last

--- Projects/purchase_order.php
+++ Projects/purchase_order.php
@@ -457,42 +457,44 @@ if (isset($_GET['bank_req'])) {
                         </tr>
 
                         <tr class="heading">
-                            <table class="table table-hover table-fixed">
+                            <div class="table-responsive p-0">
+                                <table class="table table-hover table-fixed">
 
-                                <!--Table head-->
-                                <thead class="bg-dark text-light">
-                                    <tr>
-                                        <th style="color: white;">TOTAL</th>
-                                        <th style="color: white;">UNIT PRICE</th>
-                                        <th style="color: white;">QTY</th>
-                                        <th style="color: white;">DESCRIPTION</th>
-                                   
-                                    </tr>
-                                </thead>
-                                <!--Table head-->
+                                    <!--Table head-->
+                                    <thead class="bg-dark text-light">
+                                        <tr>
+                                            <th style="color: white;">TOTAL</th>
+                                            <th style="color: white;">UNIT PRICE</th>
+                                            <th style="color: white;">QTY</th>
+                                            <th style="color: white;">DESCRIPTION</th>
 
-                                <!--Table body-->
-                                <tbody>
-                                    <tr>
-                                        <td>2.250.00</td>
-                                        <td>150.00</td>
-                                        <td>15</td>
-                                        <td>Product AAA</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>75.00</td>
-                                        <td>75.00</td>
-                                        <td>1</td>
-                                        <td>Product zzz</td>
-                                        
-                                    </tr>
-                                    
+                                        </tr>
+                                    </thead>
+                                    <!--Table head-->
 
-                                </tbody>
-                                <!--Table body-->
+                                    <!--Table body-->
+                                    <tbody>
+                                        <tr>
+                                            <td>2.250.00</td>
+                                            <td>150.00</td>
+                                            <td>15</td>
+                                            <td>Product AAA</td>
 
-                            </table>
+                                        </tr>
+                                        <tr>
+                                            <td>75.00</td>
+                                            <td>75.00</td>
+                                            <td>1</td>
+                                            <td>Product zzz</td>
+
+                                        </tr>
+
+
+                                    </tbody>
+                                    <!--Table body-->
+
+                                </table>
+                            </div>
                         </tr>
 
 
