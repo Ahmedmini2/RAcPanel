c462046da4529d3ad447ab57a59306ffaee15b06
mohammed jamal <yamni.zero@gmail.com>
2023-10-02 15:00:02 +0200

    ASSACASC

--- Projects/purchase_order.php
+++ Projects/purchase_order.php
@@ -462,7 +462,7 @@ if (isset($_GET['bank_req'])) {
                                     <tr>
                                         <th style="color: white;" scope="col">REQUISITIONER</th>
                                         <th style="color: white;" scope="col">SHIP VIA</th>
-                                        <th style="color: white;" scope="col">F.O.B</th>
+                                        <th style="color: white;"  scope="col">F.O.B</th>
                                         <th style="color: white;" Ascope="col">SHIPPING TERMS</th>
                                     </tr>
                                 </thead>
@@ -482,7 +482,7 @@ if (isset($_GET['bank_req'])) {
                                     <tr>
                                         <th style="color: white;" scope="col">REQUISITIONER</th>
                                         <th style="color: white;" scope="col">SHIP VIA</th>
-                                        <th style="color: white;" scope="col">F.O.B</th>
+                                        <th style="color: white;"  scope="col">F.O.B</th>
                                         <th style="color: white;" Ascope="col">SHIPPING TERMS</th>
                                     </tr>
                                 </thead>
@@ -493,40 +493,24 @@ if (isset($_GET['bank_req'])) {
                                         <td>Otto</td>
                                         <td>@mdo</td>
                                     </tr>
-                                    <table class="table table-sm table-dark">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">#</th>
-                                                <th scope="col">First</th>
-                                                <th scope="col">Last</th>
-                                                <th scope="col">Handle</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td colspan="2">Larry the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    
                                 </tbody>
                             </table>
                         </tr>
 
-
+                        
 
 
 
