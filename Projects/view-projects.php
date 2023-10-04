ba319bf28e2e13ef1078a4a946a8aad2fe1e670c
mohammed jamal <yamni.zero@gmail.com>
2023-10-04 15:59:20 +0200

    wqf

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -415,7 +415,7 @@ if (isset($_GET['id'])) {
                                                         <th>تم انتاج</th>
                                                         <th>تم الاستلام</th>
                                                         <th>المتبقي</th>
-                                                    
+
                                                     </tr>
                                                 </thead>
                                                 <!--Table head-->
@@ -555,6 +555,75 @@ if (isset($_GET['id'])) {
                                                 </div>
                                             </li>
                                         </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="card h-100">
+                                <div class="card-header pb-0">
+                                    <h6>نظرة عامة على الطلبات</h6>
+                                    <p class="text-sm">
+                                        <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
+                                        <span class="font-weight-bold">24%</span> هذا الشهر
+                                    </p>
+                                </div>
+                                <div class="card-body p-3">
+                                    <div class="timeline timeline-one-side">
+                                        <div class="timeline-block mb-3">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-bell-55 text-success text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, تغييرات في التصميم</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                                            </div>
+                                        </div>
+                                        <div class="timeline-block mb-3">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-html5 text-danger text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">طلب جديد #1832412</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
+                                            </div>
+                                        </div>
+                                        <div class="timeline-block mb-3">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-cart text-info text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">مدفوعات الخادم لشهر أبريل</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
+                                            </div>
+                                        </div>
+                                        <div class="timeline-block mb-3">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-credit-card text-warning text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">تمت إضافة بطاقة جديدة للطلب #4395133</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">20 DEC 2:20 AM</p>
+                                            </div>
+                                        </div>
+                                        <div class="timeline-block mb-3">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-key-25 text-primary text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">فتح الحزم من أجل التطوير</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">18 DEC 4:54 AM</p>
+                                            </div>
+                                        </div>
+                                        <div class="timeline-block">
+                                            <span class="timeline-step">
+                                                <i class="ni ni-money-coins text-dark text-gradient"></i>
+                                            </span>
+                                            <div class="timeline-content">
+                                                <h6 class="text-dark text-sm font-weight-bold mb-0">طلب جديد #9583120</h6>
+                                                <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">17 DEC</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
