7d44ba4bd79aa7e0b1bd329789a24eb734346ee7
Ahmedmini2 <liltemes@gmail.com>
2023-10-04 10:18:04 +0400

    dwa

--- Projects/purchase_order.php
+++ Projects/purchase_order.php
@@ -609,6 +609,7 @@ if (isset($_GET['bank_req'])) {
                         value =  69920
                         document.getElementById("con").value = numToWords(value);
                         console.log(value);
+                        console.log(numToWords(value));
                     }
                     window.onload=changeVal;
                 </script>
