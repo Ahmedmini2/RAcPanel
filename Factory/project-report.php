61d8f6bca80afa59b586b88bfa4274b2bb1d2169
Ahmedmini2 <liltemes@gmail.com>
2023-10-05 17:19:57 +0400

    dd

--- Factory/project-report.php
+++ Factory/project-report.php
@@ -3,6 +3,43 @@ include('../cookies/session2.php');
 $_SESSION['sidebar'] = "Factory";
 if(isset($_GET['project_id'])){
     $id = $_GET['project_id'];
+
+    if(isset($_POST['submit'])){
+
+        $str = explode(",",$_POST['name']);
+        $product_id = $str[0];
+        $product_name = $str[1];
+        $description = $_POST['description'];
+        $product_quantity = $_POST['product_quantity'];
+        $type = $_POST['type'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+        $extra = $_POST['extra'];
+
+        if($description == "صب كامل"){
+            $status = "إنتاج";
+            $insert_product_status = "INSERT INTO product_status (`id`, `product_id`, `status`, `name`, `description`, `quantity`, `kharasana_type`, `kharasana_price`, `kharasana_used`,`extra_price`,`production`,`warehouse`, `created_at`)
+            VALUES (NULL, '$product_id' ,'$status', '$product_name' , '$description' , '$product_quantity' , '$type' , '$price' ,'$quantity','$extra','$product_quantity','$product_quantity', NOW())";
+            $res = $conn->query($insert_product_status);
+        }else{
+            $status = "قيد التصنيع";
+            $insert_product_status = "INSERT INTO product_status (`id`, `product_id`, `status`, `name`, `description`, `quantity`, `kharasana_type`, `kharasana_price`, `kharasana_used`,`extra_price`,`production`,`warehouse`, `created_at`)
+          VALUES (NULL, '$product_id' ,'$status', '$product_name' , '$description' , '$product_quantity' , '$type' , '$price' ,'$quantity','$extra','0','0', NOW())";
+          $res = $conn->query($insert_product_status);
+        }
+        if ($res) {
+            $_SESSION['notification'] = "تم اضافة تقرير الانتاج بنجاح";
+            header('location: view-factory.php?id='.$id.'');
+            exit();
+          } else {
+            $_SESSION['notification'] = "خطأ في الادخال";
+            header('location: view-factory.php?id='.$id.'');
+            exit();
+          }
+
+        
+
+    }
 }
 
 ?>
@@ -176,7 +213,7 @@ if(isset($_GET['project_id'])){
                                         <?php
                                         $s_items = mysqli_query($conn, "SELECT * FROM products WHERE `project_id` = $id ");
                                         while ($item = mysqli_fetch_array($s_items)){
-                                            echo '<option value="'.$item['product_name'].'">'.$item['product_name'].'</option>';
+                                            echo '<option value="'.$item['id'].','.$item['product_name'].'">'.$item['product_name'].'</option>';
                                         }
 
                                         ?>
@@ -188,13 +225,13 @@ if(isset($_GET['project_id'])){
                             <div class="col">
                                 <div class="form-group">
                                     <label>نوع الاجراء</label>
-                                    <select name="name" id="name" class="form-control" placeholder="نوع الاجراء">
+                                    <select name="description" id="description" class="form-control" placeholder="نوع الاجراء">
                                         <option value="0"></option>
-                                        <option value="1">صب ارضية</option>
-                                        <option value="2">صب جوانب</option>
-                                        <option value="3">صب سقفية</option>
-                                        <option value="4">صب رقبية</option>
-                                        <option value="5">صب كامل</option>
+                                        <option value="صب ارضية">صب ارضية</option>
+                                        <option value="صب جوانب">صب جوانب</option>
+                                        <option value="صب سقفية">صب سقفية</option>
+                                        <option value="صب رقبية">صب رقبية</option>
+                                        <option value="صب كامل">صب كامل</option>
                                     </select>
                                 </div>
                             </div>
@@ -205,17 +242,17 @@ if(isset($_GET['project_id'])){
                             <div class="col">
                                 <div class="form-group">
                                     <label> كمية الاصناف </label>
-                                    <input type="text" placeholder="الرجاء كتابة كمية الاصناف    " class="form-control" name="branch" value="">
+                                    <input type="text" placeholder="الرجاء كتابة كمية الاصناف    " class="form-control" name="product_quantity" value="">
 
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label>نوع الخرسانة</label>
-                                    <select name="name" id="name" class="form-control" placeholder="نوع الخرسانة">
+                                    <select name="type" id="type" class="form-control" placeholder="نوع الخرسانة">
                                         <option value="0"></option>
-                                        <option value="1">خرسانة شركة</option>
-                                        <option value="2">خرسانة رجيع</option>
+                                        <option value="خرسانة شركة">خرسانة شركة</option>
+                                        <option value="خرسانة رجيع">خرسانة رجيع</option>
 
                                     </select>
                                 </div>
@@ -226,7 +263,7 @@ if(isset($_GET['project_id'])){
                             <div class="col">
                                 <div class="form-group">
                                     <label> سعر الخرسانة </label>
-                                    <input type="text" placeholder="الرجاء كتابة سعر الخرسانة " class="form-control" name="branch" value="">
+                                    <input type="text" placeholder="الرجاء كتابة سعر الخرسانة " class="form-control" name="price" value="">
 
                                 </div>
                             </div>
@@ -234,7 +271,7 @@ if(isset($_GET['project_id'])){
                             <div class="col">
                                 <div class="form-group">
                                     <label> كمية الخرسانة </label>
-                                    <input type="text" placeholder="الرجاء كتابة كمية الخرسانة " class="form-control" name="branch" value="">
+                                    <input type="text" placeholder="الرجاء كتابة كمية الخرسانة " class="form-control" name="quantity" value="">
 
                                 </div>
                             </div>
@@ -244,7 +281,7 @@ if(isset($_GET['project_id'])){
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label> مبلغ اضافي </label>
-                                    <input type="text" placeholder="الرجاء كتابة المبلغ " class="form-control" name="branch" value="">
+                                    <input type="text" placeholder="الرجاء كتابة المبلغ " class="form-control" name="extra" value="">
 
                                 </div>
                             </div>
@@ -265,72 +302,7 @@ if(isset($_GET['project_id'])){
         </div>
 
     </main>
-    <div class="fixed-plugin">
-        <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-            <i class="fa fa-cog py-2"> </i>
-        </a>
-        <div class="card shadow-lg ">
-            <div class="card-header pb-0 pt-3 ">
-                <div class="float-end">
-                    <h5 class="mt-3 mb-0">Soft UI Configurator</h5>
-                    <p>See our dashboard options.</p>
-                </div>
-                <div class="float-start mt-4">
-                    <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-                        <i class="fa fa-close"></i>
-                    </button>
-                </div>
-                <!-- End Toggle Button -->
-            </div>
-            <hr class="horizontal dark my-1">
-            <div class="card-body pt-sm-3 pt-0">
-                <!-- Sidebar Backgrounds -->
-                <div>
-                    <h6 class="mb-0">Sidebar Colors</h6>
-                </div>
-                <a href="javascript:void(0)" class="switch-trigger background-color">
-                    <div class="badge-colors my-2 text-end">
-                        <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-                        <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-                        <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-                        <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-                        <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-                        <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-                    </div>
-                </a>
-                <!-- Sidenav Type -->
-                <div class="mt-3">
-                    <h6 class="mb-0">Sidenav Type</h6>
-                    <p class="text-sm">Choose between 2 different sidenav types.</p>
-                </div>
-                <div class="d-flex">
-                    <button class="btn bg-gradient-primary w-100 px-3 mb-2 active" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-                    <button class="btn bg-gradient-primary w-100 px-3 mb-2 me-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-                </div>
-                <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p>
-                <!-- Navbar Fixed -->
-                <div class="mt-3">
-                    <h6 class="mb-0">Navbar Fixed</h6>
-                </div>
-                <div class="form-check form-switch ps-0">
-                    <input class="form-check-input mt-1 float-end me-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-                </div>
-                <hr class="horizontal dark my-sm-4">
-                <a class="btn bg-gradient-dark w-100" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro">Free Download</a>
-                <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard">View documentation</a>
-                <div class="w-100 text-center">
-                    <a class="github-button" href="https://github.com/creativetimofficial/soft-ui-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/soft-ui-dashboard on GitHub">Star</a>
-                    <h6 class="mt-3">Thank you for sharing!</h6>
-                    <a href="https://twitter.com/intent/tweet?text=Check%20Soft%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-                        <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-                    </a>
-                    <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/soft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-                        <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <!--   Core JS Files   -->
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
@@ -338,186 +310,7 @@ if(isset($_GET['project_id'])){
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script src="../assets/js/plugins/fullcalendar.min.js"></script>
     <script src="../assets/js/plugins/chartjs.min.js"></script>
-    <script>
-        var ctx = document.getElementById("chart-bars").getContext("2d");
-
-        new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Sales",
-                    tension: 0.4,
-                    borderWidth: 0,
-                    borderRadius: 4,
-                    borderSkipped: false,
-                    backgroundColor: "#fff",
-                    data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
-                    maxBarThickness: 6
-                }, ],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                        },
-                        ticks: {
-                            suggestedMin: 0,
-                            suggestedMax: 500,
-                            beginAtZero: true,
-                            padding: 15,
-                            font: {
-                                size: 14,
-                                family: "Open Sans",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                            color: "#fff"
-                        },
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false
-                        },
-                        ticks: {
-                            display: false
-                        },
-                    },
-                },
-            },
-        });
-
-
-        var ctx2 = document.getElementById("chart-line").getContext("2d");
-
-        var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
-
-        gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
-        gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
-        gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
-
-        var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
-
-        gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
-        gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
-        gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
-
-        new Chart(ctx2, {
-            type: "line",
-            data: {
-                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                        label: "Mobile apps",
-                        tension: 0.4,
-                        borderWidth: 0,
-                        pointRadius: 0,
-                        borderColor: "#cb0c9f",
-                        borderWidth: 3,
-                        backgroundColor: gradientStroke1,
-                        fill: true,
-                        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
-                        maxBarThickness: 6
-
-                    },
-                    {
-                        label: "Websites",
-                        tension: 0.4,
-                        borderWidth: 0,
-                        pointRadius: 0,
-                        borderColor: "#3A416F",
-                        borderWidth: 3,
-                        backgroundColor: gradientStroke2,
-                        fill: true,
-                        data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
-                        maxBarThickness: 6
-                    },
-                ],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            padding: 10,
-                            color: '#b2b9bf',
-                            font: {
-                                size: 11,
-                                family: "Open Sans",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            color: '#b2b9bf',
-                            padding: 20,
-                            font: {
-                                size: 11,
-                                family: "Open Sans",
-                                style: 'normal',
-                                lineHeight: 2
-                            },
-                        }
-                    },
-                },
-            },
-        });
-    </script>
-    <script src="../assets/js/plugins/choices.min.js"></script>
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
+    
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
