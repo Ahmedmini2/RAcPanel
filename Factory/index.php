2b16100d414defacc16a76aca1b184bab8d37393
mohammed jamal <yamni.zero@gmail.com>
2023-10-05 10:02:37 +0200

    ش

--- Factory/index.php
+++ Factory/index.php
@@ -215,37 +215,16 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
             </div>
             <div class="col-12 col-md-6 col-lg-3">
                 <div class="card">
-
-                    <img class="card-img-top" src="https://www.bootdey.com/image/350x280/87CEFA/000000" alt="Unsplash">
+                 
+                    <img class="card-img-top" src="../assets/img/home-decor-3.jpg" alt="img-blur-shadow">
 
                     <div class="card-header px-4 pt-4">
-                        <div class="card-actions float-right">
-                            <div class="dropdown show">
-                                <a href="#" data-toggle="dropdown" data-display="static">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal align-middle">
-                                        <circle cx="12" cy="12" r="1"></circle>
-                                        <circle cx="19" cy="12" r="1"></circle>
-                                        <circle cx="5" cy="12" r="1"></circle>
-                                    </svg>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </div>
-                        </div>
-                        <h5 class="card-title mb-0">Fix form validation</h5>
+                    <p class="text-gradient text-dark mb-2 text-sm">المشروع رقم :2</p>
+                         <h5>مشروع اراميكس</h5>
                         <div class="badge bg-warning my-2">In progress</div>
                     </div>
                     <div class="card-body px-4 pt-2">
-                        <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam
-                            ultrices mauris.</p>
-
-                        <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
+                        <p>توصيل سيارات</p>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item px-4 pb-4">
