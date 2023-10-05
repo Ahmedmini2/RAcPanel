8896569784527f9e6e720a87647491f1624362df
Ahmedmini2 <liltemes@gmail.com>
2023-10-05 17:26:10 +0400

    daw

--- Factory/view-factory.php
+++ Factory/view-factory.php
@@ -172,7 +172,7 @@ if (isset($_GET['id'])) {
 
         <div class="container-fluid py-4">
             <div class="row">
-                <div class="block-header bg-gradient-dark  col-md-3 col-sm-6 col-xs-6  rounded-pill">
+                <div class="">
 
                     <?php require_once('../components/notification.php'); ?>
                 </div>
