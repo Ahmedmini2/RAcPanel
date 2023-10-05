d05e448c3272817a655689b8d60f967a732fa9db
mohammed jamal <yamni.zero@gmail.com>
2023-10-05 11:24:49 +0200

    p-r

--- Factory/project-report.php
+++ Factory/project-report.php
@@ -198,52 +198,23 @@ if (!empty($_GET['edit'])) {
         <!-- End Navbar -->
         <div class="container-fluid py-4">
             <div class="row">
-                <div class="col">
-                    <div class="form-group">
-                        <label>اختيار الصنف </label>
-                        <select class="select" placeholder="الصنف">
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <footer class="footer pt-3  ">
-            <div class="container-fluid">
-                <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-sm text-muted text-lg-end">
-                            © <script>
-                                document.write(new Date().getFullYear())
-                            </script>,
-                            made with <i class="fa fa-heart"></i> by
-                            <a href="" class="font-weight-bold" target="_blank">Rukn Amial</a>
-
+                <div class="block block-themed">
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <label>اختيار الصنف </label>
+                                <select class="select" placeholder="الصنف">
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                            <li class="nav-item">
-                                <a href="https://ruknamial.com" class="nav-link text-muted" target="_blank">Rukn Amial</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://files.ruknamial.com" class="nav-link text-muted" target="_blank">Files</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://ruknamial.com/blogs" class="nav-link text-muted" target="_blank">Blog</a>
-                            </li>
-
-                        </ul>
-                    </div>
                 </div>
             </div>
-        </footer>
         </div>
+    </div>
     </main>
     <div class="fixed-plugin">
         <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
