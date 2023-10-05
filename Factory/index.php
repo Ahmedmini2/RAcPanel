27d3a70e75b46039d9a0911cdd7d69b4594185cb
mohammed jamal <yamni.zero@gmail.com>
2023-10-05 09:57:26 +0200

    trfactory -view

--- Factory/index.php
+++ Factory/index.php
@@ -173,7 +173,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
                         <h3 class="mb-1">المصنع</h3>
                     </div>
 
-                    
+
 
 
                     <div class="card-body p-3  ">
@@ -211,6 +211,51 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
                             ?>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+
+                    <img class="card-img-top" src="https://www.bootdey.com/image/350x280/87CEFA/000000" alt="Unsplash">
+
+                    <div class="card-header px-4 pt-4">
+                        <div class="card-actions float-right">
+                            <div class="dropdown show">
+                                <a href="#" data-toggle="dropdown" data-display="static">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal align-middle">
+                                        <circle cx="12" cy="12" r="1"></circle>
+                                        <circle cx="19" cy="12" r="1"></circle>
+                                        <circle cx="5" cy="12" r="1"></circle>
+                                    </svg>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title mb-0">Fix form validation</h5>
+                        <div class="badge bg-warning my-2">In progress</div>
+                    </div>
+                    <div class="card-body px-4 pt-2">
+                        <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam
+                            ultrices mauris.</p>
+
+                        <img src="https://bootdey.com/img/Content/avatar/avatar3.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar2.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar1.png" class="rounded-circle mr-1" alt="Avatar" width="28" height="28">
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item px-4 pb-4">
+                            <p class="mb-2 font-weight-bold">Progress <span class="float-right">65%</span></p>
+                            <div class="progress progress-sm">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <footer class="footer pt-3  ">
