4ecd43f8d158662ae8c602ea125819d4551395ab
Ahmedmini2 <liltemes@gmail.com>
2023-10-08 12:09:09 +0400

    gg

--- Factory/index.php
+++ Factory/index.php
@@ -185,7 +185,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
               <div class="card card-blog card-plain py-3">
                 <div class="position-relative">
                   <a class="d-block shadow-xl border-radius-xl">
-                    <img src="Images/' . $r['name'] . '/' . $r['image'] . '" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl inside-card">
+                    <img src="../Projects/Images/' . $r['name'] . '/' . $r['image'] . '" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl inside-card">
                   </a>
                 </div>
                 <div class="card-body px-1 pb-0">
