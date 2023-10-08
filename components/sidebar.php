4f488d1b95532c6185997364aa70992643ce64e6
Ahmedmini2 <liltemes@gmail.com>
2023-10-08 11:12:35 +0400

    ss

--- components/sidebar.php
+++ components/sidebar.php
@@ -158,7 +158,7 @@ $sidebar = $_SESSION['sidebar'];
       </li>
       <ul class="navbar-nav active">
         <li class="nav-item ">
-          <a href="#submenu1" data-bs-toggle="collapse" class="nav-link <?php if ($sidebar == "Cost") echo 'active'; ?>"">
+          <a href="#submenu2" data-bs-toggle="collapse" class="nav-link <?php if ($sidebar == "Cost") echo 'active'; ?>"">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>shop </title>
@@ -176,7 +176,7 @@ $sidebar = $_SESSION['sidebar'];
             </div>
             <span class="ms-1  d-sm-inline">مركز التكلفة</span>
           </a>
-          <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+          <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
             <li class="w-100">
               <a href="../Cost/cost.php" class="nav-link px-0"> <span class=" d-sm-inline">تكلفة</span></a>
             </li>
