7d464709f5255eedb199de5146326818baaa5ca4
mohammed jamal <yamni.zero@gmail.com>
2023-10-08 11:28:33 +0200

    s

--- cost/type-cost.php
+++ cost/type-cost.php
@@ -154,7 +154,7 @@ $select =mysqli_query($conn, "select * from beneficiary_info");
     <!-- End Navbar -->
     <div class="container-fluid py-4">
         <div class="row">
-        <a href="add-beneficiary-bank.php"
+        <a href="add-type-cost.php"
          class="btn bg-gradient-dark mb-0 col-md-2 col-sm-6 col-xs-6">نوع تكلفة جديد&nbsp;&nbsp; 
          <i class="fas fa-plus">
          </i>
