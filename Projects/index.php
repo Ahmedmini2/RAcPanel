4ea47c5c231026189e4d9d9e463c8567bf7853bb
Ahmedmini2 <liltemes@gmail.com>
2023-10-08 12:05:39 +0400

    ff

--- Projects/index.php
+++ Projects/index.php
@@ -189,7 +189,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
               <div class="card card-blog card-plain py-3">
                 <div class="position-relative">
                   <a class="d-block shadow-xl border-radius-xl">
-                    <img src="Images/' . $r['name'] . '/' . $r['image'] . '">
+                    <img class="inside-card" src="Images/' . $r['name'] . '/' . $r['image'] . '">
                   </a>
                 </div>
                 <div class="card-body px-1 pb-0">
