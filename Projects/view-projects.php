635da7c81845d6a5f43f8a629df9dc5b9574a184
Ahmedmini2 <liltemes@gmail.com>
2023-10-09 22:38:08 +0400

    awda

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -455,9 +455,6 @@ if (isset($_GET['id'])) {
                                                                 $production += $inv['production'];
                                                             }
                                                             ?>
-                                                            <td><?= number_format($inventory) ?></td>
-                                                            <td><?= number_format($production) ?></td>
-
                                                             <?php 
                                                             $del_id =  $products['id'];
                                                             $del_res = mysqli_query($conn, "SELECT * FROM product_delivery WHERE `product_id` = $del_id");
@@ -468,8 +465,12 @@ if (isset($_GET['id'])) {
                                                                 
                                                             }
                                                             ?>
+                                                            <td><?= number_format($inventory) ?></td>
+                                                            <td><?= number_format($production - $deliverd) ?></td>
 
-                                                            <td><?= number_format($deliverd - $production) ?></td>
+                                                            
+
+                                                            <td><?= number_format($deliverd) ?></td>
 
                                                             <td><?=$products['quantity']-$production?></td>
                                                         </tr>
