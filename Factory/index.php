864cbb9e9dd9a3734f7fd901a63f36f14e3b9f4c
Ahmedmini2 <liltemes@gmail.com>
2023-10-09 12:27:03 +0400

    ss

--- Factory/index.php
+++ Factory/index.php
@@ -184,7 +184,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
                                 <?php
                                 while ($r = mysqli_fetch_array($projects)) {
 
-                                    echo '<div class="col-xs-12 col-sm-6 col-md-4">
+                                    echo '<div class="col-xs-12 col-sm-6 col-md-4 pt-2">
                         <div class="card">
 
                          
