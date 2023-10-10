6f718ffb5f4e6ee7a6304401acd651e35a1a87ea
mohammed jamal <yamni.zero@gmail.com>
2023-10-10 12:38:29 +0200

    aw

--- Projects/index.php
+++ Projects/index.php
@@ -189,7 +189,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
 
                 echo '<div class="col-xs-12 col-sm-6 col-md-4 pt-2">
                     
-                        <div class="card " style="width:18rem; height:20rem">
+                        <div class="card ">
 
                          
                             <div class="view overlay">
