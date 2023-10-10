eb91e818aa19c4e3c4b5e1e09cd5fc1600f0c320
Ahmedmini2 <liltemes@gmail.com>
2023-10-10 17:30:47 +0400

    Final

--- cost/type-cost.php
+++ cost/type-cost.php
@@ -170,7 +170,7 @@ $select = mysqli_query($conn, "SELECT * from cost_type");
 
               <table class="table align-items-center mb-0" id="myTable">
                 <thead>
-                  <tr>
+                  <tr class="text-center">
                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"  width="2%">الرقم</th>
                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"  width="10%">الأسم</th>
                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">انشاء بتاريخ</th>
