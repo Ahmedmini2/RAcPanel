6a5ee525703563768c86f0d0ad919f6dda2db65c
mohammed jamal <yamni.zero@gmail.com>
2023-10-11 09:42:32 +0200

    csa

--- Projects/index.php
+++ Projects/index.php
@@ -208,16 +208,18 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
                                 
                                 <p class="card-text">' . $r["description"] . '</p>
                                 
-                                <a href="view-projects.php?id=' . $r["id"] . '"> 
-                                    <div class="d-flex align-items-center justify-content-between">
-                                        <button  type="button" class="btn btn-outline-primary  btn-sm mb-0">عرض التفاصيل</button>
-                                    </div>
-                                </a>
+                                
 
                             </div>
-                            <hr>
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <button  type="button" class="btn btn-outline-primary  btn-sm mb-0">عرض التفاصيل</button>
+                                <div class="text-center">
+                                <a href="view-projects.php?id=' . $r["id"] . '"> 
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <button  type="button" class="btn btn-outline-primary  btn-sm mb-0">عرض التفاصيل</button>
+                                </div>
+                            </a>
+                                </div>
+                            </div>
 
                           </div>
                         
