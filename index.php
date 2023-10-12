adf3b65ecb5d1242c252ec5042b025e3082b2add
Ahmedmini2 <liltemes@gmail.com>
2023-10-12 13:36:58 +0400

    fff

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 include ('cookies/session.php');
 $_SESSION['sidebar']="Home";
 
+$show_products_status = mysqli_query($conn, "SELECT * FROM `product_status`");
+
 ?>
 
 <html lang="ar" dir="rtl">
@@ -611,68 +613,42 @@ $_SESSION['sidebar']="Home";
         <div class="col-lg-4 col-md-6">
           <div class="card h-100">
             <div class="card-header pb-0">
-              <h6>نظرة عامة على الطلبات</h6>
+              <h6>نظرة عامة على سير العمل</h6>
               <p class="text-sm">
-                <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
-                <span class="font-weight-bold">24%</span> هذا الشهر
+                
+                <span class="font-weight-bold"></span> في الأيام الماضية
               </p>
             </div>
             <div class="card-body p-3">
               <div class="timeline timeline-one-side">
+                <?php
+                 while ($r = mysqli_fetch_array($select)) {
+                  $product_id = $r['product_id'];
+                  $query = "SELECT * FROM `products` WHERE `id`=$product_id";
+                  $res = $conn->query($query);
+                  $product = $res->fetch_assoc();
+
+                  $project_id = $product['project_id'];
+                  $query = "SELECT * FROM `products` WHERE `id`=$product_id";
+                  $res = $conn->query($query);
+                  $project = $res->fetch_assoc();
+
+                  $date = new DateTimeImmutable($r['created_at']);
+
+                  
+                  ?>     
                 <div class="timeline-block mb-3">
                   <span class="timeline-step">
-                    <i class="ni ni-bell-55 text-success text-gradient"></i>
+                    <i class="fa fa-bell text-success text-gradient"></i>
                   </span>
                   <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">$2400, تغييرات في التصميم</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">22 DEC 7:20 PM</p>
+                    
+                    <h6 class="text-dark text-sm font-weight-bold mb-0">في مشروع <?=$project['name']?> تم <?=$r['description']?> وحالته <?=$r['status']?></h6>
+                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0"><?=$date->format(DateTimeInterface::RFC2822)?></p>
                   </div>
                 </div>
-                <div class="timeline-block mb-3">
-                  <span class="timeline-step">
-                    <i class="ni ni-html5 text-danger text-gradient"></i>
-                  </span>
-                  <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">طلب جديد #1832412</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 11 PM</p>
-                  </div>
-                </div>
-                <div class="timeline-block mb-3">
-                  <span class="timeline-step">
-                    <i class="ni ni-cart text-info text-gradient"></i>
-                  </span>
-                  <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">مدفوعات الخادم لشهر أبريل</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">21 DEC 9:34 PM</p>
-                  </div>
-                </div>
-                <div class="timeline-block mb-3">
-                  <span class="timeline-step">
-                    <i class="ni ni-credit-card text-warning text-gradient"></i>
-                  </span>
-                  <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">تمت إضافة بطاقة جديدة للطلب #4395133</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">20 DEC 2:20 AM</p>
-                  </div>
-                </div>
-                <div class="timeline-block mb-3">
-                  <span class="timeline-step">
-                    <i class="ni ni-key-25 text-primary text-gradient"></i>
-                  </span>
-                  <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">فتح الحزم من أجل التطوير</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">18 DEC 4:54 AM</p>
-                  </div>
-                </div>
-                <div class="timeline-block">
-                  <span class="timeline-step">
-                    <i class="ni ni-money-coins text-dark text-gradient"></i>
-                  </span>
-                  <div class="timeline-content">
-                    <h6 class="text-dark text-sm font-weight-bold mb-0">طلب جديد #9583120</h6>
-                    <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">17 DEC</p>
-                  </div>
-                </div>
+
+                <?php } ?>
               </div>
             </div>
           </div>
