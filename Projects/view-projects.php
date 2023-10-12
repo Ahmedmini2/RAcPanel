8cae491d14fe494eea04bf0fc2bdc85abffc3f7e
Ahmedmini2 <liltemes@gmail.com>
2023-10-12 14:51:02 +0400

    aa

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -557,7 +557,7 @@ if (isset($_GET['id'])) {
                             </div>
                         </div>
 
-                        <div class="col-lg-4 col-md-6 my-4">
+                        <div class="col-lg-16 col-md-6 my-4">
                             <div class="card h-100">
                                 <div class="card-header pb-0">
                                     <h6>نظرة عامة على سير العمل</h6>
