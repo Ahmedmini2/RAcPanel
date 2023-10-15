73f87b9c919f02944b6c3698b6d6031226f0459c
mohammed jamal <yamni.zero@gmail.com>
2023-10-15 14:41:37 +0200

    acsa

--- components/sidebar.php
+++ components/sidebar.php
@@ -223,7 +223,7 @@ $sidebar = $_SESSION['sidebar'];
       <?php } ?>
       <?php if ($position == 'Admin' || $position == 'Worker') { ?> 
       <li class="nav-item">
-        <a class="nav-link <?php if ($sidebar == "Cover") echo 'active'; ?>" href="../View/index_view.php">
+        <a class="nav-link <?php if ($sidebar == "View") echo 'active'; ?>" href="../View/index_view.php">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center ms-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <title>document</title>
