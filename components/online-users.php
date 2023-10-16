6f5fd8c5917d5af1960b232aea90f3e9ffe351f2
Ahmedmini2 <liltemes@gmail.com>
2023-10-16 11:28:20 +0400

    dwa

---
+++ components/online-users.php
@@ -0,0 +1,16 @@
+<div class="col-md-1">
+    <div class="widget">
+        <div class="widget-content">
+            <ul class="widget-user-list">
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt=""></a></li>
+                <li><a href="#"><img src="https://bootdey.com/img/Content/avatar/avatar5.png" alt=""></a></li>
+                <li class="number"><a href="#">+26</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
