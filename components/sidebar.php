59b04c8b26389fc401aff7afb755f43719ed311f
Ahmedmini2 <liltemes@gmail.com>
2023-10-16 11:16:08 +0400

    يصش

--- components/sidebar.php
+++ components/sidebar.php
@@ -251,16 +251,15 @@ $sidebar = $_SESSION['sidebar'];
                 <div class="dropdown pb-4">
                     <a href="#" class="d-flex align-items-center text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                        <span class="d-none d-sm-inline mx-1">loser</span>
+                        <span class="d-none d-sm-inline mx-1">عباس الجعفري</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">الرسائل</a></li>
+                        <li><a class="dropdown-item" href="#">الملف الشخصي</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        <li><a class="dropdown-item" href="#">تسجيل الخروج</a></li>
                     </ul>
                 </div>
   </div>
