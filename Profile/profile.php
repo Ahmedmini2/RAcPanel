ffdf3afd1c9fd9a43669ba6730994303a70d4b65
mohammed jamal <yamni.zero@gmail.com>
2023-10-16 09:42:46 +0200

    aC

--- Profile/profile.php
+++ Profile/profile.php
@@ -175,7 +175,7 @@ $select =mysqli_query($conn, "select * from users");
             </div>
           </div>
           <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
-            <div class="nav-wrapper position-relative end-0">
+            <div class="nav-wrapper position-relative end-0" dir="ltr">
               <ul class="nav nav-pills nav-fill p-1 bg-transparent" role="tablist">
                 <li class="nav-item">
                   <a class="nav-link mb-0 px-0 py-1 active " data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
