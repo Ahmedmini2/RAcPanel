6153c33630e2112a3e2bc3d825b8be954d38c498
mohammed jamal <yamni.zero@gmail.com>
2023-10-17 12:13:46 +0200

    sacc

--- Projects/view-projects.php
+++ Projects/view-projects.php
@@ -533,7 +533,7 @@ if (isset($_GET['id'])) {
                                                                     <th>طول الحديد</th>
                                                                     <th>سعر الطن</th>
                                                                     <th>السعر الكلي</th>
-                                                                    <th>السعر الكلي</th>
+                                                                    
 
                                                                 </tr>
                                                             </thead>
@@ -559,7 +559,7 @@ if (isset($_GET['id'])) {
                                                                     </tr>
                                                                 <?php } ?>
                                                                     <tr>
-                                                                    <td><?= $iron['size'] ?></td>
+                                                                    <td>8222</td>
                                                                     </tr>
 
                                                             </tbody>
