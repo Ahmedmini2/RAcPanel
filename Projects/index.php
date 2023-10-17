9744c94c1ca3beaf51fb8aacbb3873cc390b42d0
Ahmedmini2 <liltemes@gmail.com>
2023-10-17 13:02:43 +0400

    dwa

--- Projects/index.php
+++ Projects/index.php
@@ -194,7 +194,7 @@ $projects = mysqli_query($conn, "SELECT * FROM projects");
                          
                             <div class="view overlay">
                                 <img class="inside-card card-img-top" src="../Projects/Images/' . $r['name'] . '/' . $r['image'] . '" alt="Card image cap">
-                                <span class="card__ribbon"></span>
+                                <span class="ribbon-pop"></span>
                                     <div class=" rgba-white-slight"></div>
                                 
                             </div>
