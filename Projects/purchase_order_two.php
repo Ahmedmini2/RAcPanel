28d0e6b71c1254ffe4568da9900b2269b9fd2d91
Ahmedmini2 <liltemes@gmail.com>
2023-10-19 14:36:10 +0400

    dd

--- Projects/purchase_order_two.php
+++ Projects/purchase_order_two.php
@@ -483,7 +483,7 @@ if (isset($_GET['project_id'])) {
                 <div class="row justify-content-center">
                     <div class="col-12">
                         <div class="table-responsive p-0">
-                            <table class="table table-hover table-fixed text-center">
+                            <table class="table table-hover table-fixed text-center table-bordered">
 
                                 <!--Table head-->
                                 <thead class="text-light header-color custom-font-m">
