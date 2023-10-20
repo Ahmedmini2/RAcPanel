f11f6863cf3e98eb9b4b38e4d9d14f38a35edc0a
mohammed jamal <yamni.zero@gmail.com>
2023-10-20 16:43:45 +0200

    asc

---
+++ Admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <title>DashBoard ruknamial</title>
+</head>
+<body>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <a href="#" class="logo">
+        <i class='bx bx-code-alt'></i>
+        <div class="logo-name"><span>ركن</span>اميال</div>
+        </a>
+        <ul class="side-menu">
+            <li><a href="#"><i class='bx bxs-dashboard' ></i>لوحة القيادة</a></li>
+            
+            <li><a href="#"><i class='bx bxs-user-detail' ></i>قسم الموظفين</a></li>
+            <li><a href="#"><i class='bx bxs-user-detail' ></i>department section</a></li>
+            <li><a href="#"><i class='bx bx-log-out' ></i>نوع الاجازه</a></li>
+            <li><a href="#"><i class='bx bx-cog' ></i>اداره الاجازه</a></li>
+            <li><a href="#"><i class='bx bx-cog' ></i>الاداره</a></li>
+        </ul>
+        <ul class="side-menu">
+             <li>
+                <a href="#" class="logout">
+                <i class='bx bx-log-out-circle' ></i>
+                logout
+                </a>
+             </li>
+        </ul>
+    </div>
+    
+    <script src="index.js"></script>
+</body>
+</html>
