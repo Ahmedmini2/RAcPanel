ddfc6d8b911d24ec1a77adc63bd1d75d0ca11a99
mohammed jamal <yamni.zero@gmail.com>
2023-10-21 15:52:05 +0200

    sac

--- Admin/Test/index.php
+++ Admin/Test/index.php
@@ -18,8 +18,8 @@
 	<div class="main-wrapper">
 		<div class="header">
 			<div class="header-left">
-				
-				<a href="https://test.app.ruknamial.com/index.php" class="logo logo-small"> <img src="../assets/img/logos/logo-gold.png" alt="Logo" width="30" height="30"> </a>
+				<a href="index.html" class="logo"> <img src="assets/img/hotel_logo.png" width="50" height="70" alt="logo"> <span class="logoclass">HOTEL</span> </a>
+				<a href="index.html" class="logo logo-small"> <img src="assets/img/hotel_logo.png" alt="Logo" width="30" height="30"> </a>
 			</div>
 			<a href="javascript:void(0);" id="toggle_btn"> <i class="fe fe-text-align-left"></i> </a>
 			<a class="mobile_btn" id="mobile_btn"> <i class="fas fa-bars"></i> </a>
@@ -128,98 +128,19 @@
 						<li class="submenu"> <a href="#"><i class="fas fa-share-alt"></i> <span> Apps </span> <span class="menu-arrow"></span></a>
 							<ul class="submenu_class" style="display: none;">
 								<li><a href="chat.html"><i class="fas fa-comments"></i><span> Chat </span></a></li>
-							
-								<li class="submenu"> <a href="#"><i class="fas fa-envelope"></i> <span> Email </span> <span class="menu-arrow"></span></a>
-									
-								</li>
+								
+								
 							</ul>
 						</li>
-						<li class="submenu"> <a href="#"><i class="fas fa-user"></i> <span> Employees </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="employees.php">Employees List </a></li>
-								<li><a href="leaves.php">Leaves </a></li>
-								<li><a href="holidays.php">Holidays </a></li>
-								<li><a href="attendance.php">Attendance </a></li>
-							</ul>
-						</li>
-						<li class="submenu"> <a href="#"><i class="far fa-money-bill-alt"></i> <span> Accounts </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="invoices.html">Invoices </a></li>
-								<li><a href="payments.html">Payments </a></li>
-								<li><a href="expenses.html">Expenses </a></li>
-								<li><a href="taxes.html">Taxes </a></li>
-								<li><a href="provident-fund.html">Provident Fund </a></li>
-							</ul>
-						</li>
-						<li class="submenu"> <a href="#"><i class="fas fa-book"></i> <span> Payroll </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="salary.html">Employee Salary </a></li>
-								<li><a href="salary-veiw.html">Payslip </a></li>
-							</ul>
-						</li>
-						<li> <a href="calendar.html"><i class="fas fa-calendar-alt"></i> <span>Calendar</span></a> </li>
-						<li class="submenu"> <a href="#"><i class="fe fe-table"></i> <span> Blog </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="blog.html">Blog </a></li>
-								<li><a href="blog-details.html">Blog Veiw </a></li>
-								<li><a href="add-blog.html">Add Blog </a></li>
-								<li><a href="edit-blog.html">Edit Blog </a></li>
-							</ul>
-						</li>
-						<li> <a href="assets.html"><i class="fas fa-cube"></i> <span>Assests</span></a> </li>
-						<li> <a href="activities.html"><i class="far fa-bell"></i> <span>Activities</span></a> </li>
-						<li class="submenu"> <a href="#"><i class="fe fe-table"></i> <span> Reports </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="expense-reports.html">Expense Report </a></li>
-								<li><a href="invoice-reports.html">Invoice Report </a></li>
-							</ul>
-						</li>
-						<li> <a href="settings.html"><i class="fas fa-cog"></i> <span>Settings</span></a> </li>
-						<li class="list-divider"></li>
-						<li class="menu-title mt-3"> <span>UI ELEMENTS</span> </li>
-						<li class="submenu"> <a href="#"><i class="fas fa-laptop"></i> <span> Components </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="uikit.html">UI Kit </a></li>
-								<li><a href="typography.html">Typography </a></li>
-								<li><a href="tabs.html">Tabs </a></li>
-							</ul>
-						</li>
-						<li class="submenu"> <a href="#"><i class="fas fa-edit"></i> <span> Forms </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="form-basic-inputs.html">Basic Input </a></li>
-								<li><a href="form-input-groups.html">Input Groups </a></li>
-								<li><a href="form-horizontal.html">Horizontal Form </a></li>
-								<li><a href="form-vertical.html">Vertical Form </a></li>
-							</ul>
-						</li>
-						<li class="submenu"> <a href="#"><i class="fas fa-table"></i> <span> Tables </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="tables-basic.html">Basic Table </a></li>
-								<li><a href="tables-datatables.html">Data Table </a></li>
-							</ul>
-						</li>
-						<li class="list-divider"></li>
-						<li class="menu-title mt-3"> <span>EXTRAS</span> </li>
-						<li class="submenu"> <a href="#"><i class="fas fa-columns"></i> <span> Pages </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="login.html">Login </a></li>
-								<li><a href="register.html">Register </a></li>
-								<li><a href="forgot-password.html">Forgot Password </a></li>
-								<li><a href="change-password.html">Change Password </a></li>
-								<li><a href="lock-screen.html">Lockscreen </a></li>
-								<li><a href="profile.html">Profile </a></li>
-								<li><a href="gallery.html">Gallery </a></li>
-								<li><a href="error-404.html">404 Error </a></li>
-								<li><a href="error-500.html">500 Error </a></li>
-								<li><a href="blank-page.html">Blank Page </a></li>
-							</ul>
-						</li>
-						<li class="submenu"> <a href="#"><i class="fas fa-share-alt"></i> <span> Multi Level </span> <span class="menu-arrow"></span></a>
-							<ul class="submenu_class" style="display: none;">
-								<li><a href="">Level 1 </a></li>
-								<li><a href="">Level 2 </a></li>
-							</ul>
-						</li>
+						
+						
+						
+						
+						
+						
+						
+						
+						
 					</ul>
 				</div>
 			</div>
@@ -229,7 +150,7 @@
 				<div class="page-header">
 					<div class="row">
 						<div class="col-sm-12 mt-5">
-							<h3 class="page-title mt-3">Good Morning Soeng Souy!</h3>
+							<h3 class="page-title mt-3">Good Morning </h3>
 							<ul class="breadcrumb">
 								<li class="breadcrumb-item active">Dashboard</li>
 							</ul>
@@ -242,8 +163,8 @@
 							<div class="card-body">
 								<div class="dash-widget-header">
 									<div>
-										<h3 class="card_widget_header">236</h3>
-										<h6 class="text-muted">Total Booking</h6> </div>
+										<h3 class="card_widget_header">12</h3>
+										<h6 class="text-muted">Available Leave Type</h6> </div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <span class="opacity-7 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-plus">
 									<path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
 									<circle cx="8.5" cy="7" r="4"></circle>
@@ -259,8 +180,8 @@
 							<div class="card-body">
 								<div class="dash-widget-header">
 									<div>
-										<h3 class="card_widget_header">180</h3>
-										<h6 class="text-muted">Available Rooms</h6> </div>
+										<h3 class="card_widget_header">4</h3>
+										<h6 class="text-muted">Pening Application</h6> </div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <span class="opacity-7 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign">
 									<line x1="12" y1="1" x2="12" y2="23"></line>
 									<path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path>
@@ -274,8 +195,8 @@
 							<div class="card-body">
 								<div class="dash-widget-header">
 									<div>
-										<h3 class="card_widget_header">1538</h3>
-										<h6 class="text-muted">Enquiry</h6> </div>
+										<h3 class="card_widget_header">2</h3>
+										<h6 class="text-muted">Declined Application</h6> </div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <span class="opacity-7 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-plus">
 									<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z">
 									</path>
@@ -292,8 +213,8 @@
 							<div class="card-body">
 								<div class="dash-widget-header">
 									<div>
-										<h3 class="card_widget_header">364</h3>
-										<h6 class="text-muted">Collections</h6> </div>
+										<h3 class="card_widget_header">6</h3>
+										<h6 class="text-muted">Approved Application</h6> </div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <span class="opacity-7 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24" fill="none" stroke="#009688" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe">
 									<circle cx="12" cy="12" r="10"></circle>
 									<line x1="2" y1="12" x2="22" y2="12"></line>
@@ -305,31 +226,12 @@
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12 col-lg-6">
-						<div class="card card-chart">
-							<div class="card-header">
-								<h4 class="card-title">VISITORS</h4> </div>
-							<div class="card-body">
-								<div id="line-chart"></div>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-12 col-lg-6">
-						<div class="card card-chart">
-							<div class="card-header">
-								<h4 class="card-title">ROOMS BOOKED</h4> </div>
-							<div class="card-body">
-								<div id="donut-chart"></div>
-							</div>
-						</div>
-					</div>
-				</div>
+				
 				<div class="row">
 					<div class="col-md-12 d-flex">
 						<div class="card card-table flex-fill">
 							<div class="card-header">
-								<h4 class="card-title float-left mt-2">Booking</h4>
+								<h4 class="card-title float-left mt-2">Leave</h4>
 								<button type="button" class="btn btn-primary float-right veiwbutton">Veiw All</button>
 							</div>
 							<div class="card-body">
@@ -337,28 +239,23 @@
 									<table class="table table-hover table-center">
 										<thead>
 											<tr>
-												<th>Booking ID</th>
-												<th>Name</th>
-												<th>Email ID</th>
-												<th>Aadhar Number</th>
-												<th class="text-center">Room Type</th>
-												<th class="text-right">Number</th>
-												<th class="text-center">Status</th>
+											<th>S.N</th>
+                              					  <th>User</th>
+                                	<th>Leave type</th>
+                               	 <th>Order Date</th>
+                                <th>Status</th>
 											</tr>
 										</thead>
 										<tbody>
 											<tr>
 												<td class="text-nowrap">
-													<div>BKG-0001</div>
+													<div>1</div>
 												</td>
-												<td class="text-nowrap">Tommy Bernal</td>
-												<td><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="3743585a5a4e55524559565b77524f565a475b521954585a">[email&#160;protected]</a></td>
-												<td>12414786454545</td>
-												<td class="text-center">Double</td>
-												<td class="text-right">
-													<div>631-254-6480</div>
-												</td>
-												<td class="text-center"> <span class="badge badge-pill bg-success inv-badge">INACTIVE</span> </td>
+												
+                                <td>Mohammed</td>
+                                <td>Medical Leave</td>
+                                <td>14-10-2023</td>
+												<td class="text-center"> <span class="badge badge-pill bg-success inv-badge">Pending</span> </td>
 											</tr>
 											<tr>
 												<td class="text-nowrap">
