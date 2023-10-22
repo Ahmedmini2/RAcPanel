9d2c6676c63719f03187f1877b96d657a2a9fb25
mohammed jamal <yamni.zero@gmail.com>
2023-10-22 09:40:49 +0200

    sac

--- Admin/index.php
+++ Admin/index.php
@@ -1,5 +1,5 @@
 <?php
-
+include('../cookies/session2.php');
 $_SESSION['sidebar_admin'] = "dashboard";
 
 
@@ -28,8 +28,8 @@ $_SESSION['sidebar_admin'] = "dashboard";
 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
   <title>
     ركن أميال | Rukn Amial
   </title>
@@ -38,15 +38,15 @@ $_SESSION['sidebar_admin'] = "dashboard";
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700&display=swap" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
-  <link href="assets/css/custom.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="../assets/css/custom.css" rel="stylesheet" />
 
   <!-- CSS Files -->
-  <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <link id="pagestyle" href="../../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
 
 </head>
 
@@ -262,14 +262,14 @@ $_SESSION['sidebar_admin'] = "dashboard";
   
 
   <!--   Core JS Files   -->
-  <script src="assets/js/core/popper.min.js"></script>
-  <script src="assets/js/core/bootstrap.min.js"></script>
-  <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
-  <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script src="assets/js/plugins/fullcalendar.min.js"></script>
-  <script src="assets/js/plugins/chartjs.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/fullcalendar.min.js"></script>
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
 
-  <script src="assets/js/plugins/choices.min.js"></script>
+  <script src="../assets/js/plugins/choices.min.js"></script>
   <script>
     var win = navigator.platform.indexOf('Win') > -1;
     if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -282,7 +282,7 @@ $_SESSION['sidebar_admin'] = "dashboard";
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+  <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script>
     // Function to fetch notifications from the server
@@ -348,7 +348,7 @@ $_SESSION['sidebar_admin'] = "dashboard";
           console.log(response.message);
 
 
-          
+
           // Refresh the notifications to reflect the updated status
           fetchNotifications();
         },
