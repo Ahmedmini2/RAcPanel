6c6ab4540f74a79b914a382bbc86fa0a453a2c11
mohammed jamal <yamni.zero@gmail.com>
2023-10-23 09:00:16 +0200

    sa

--- Admin/index.php
+++ Admin/index.php
@@ -262,10 +262,20 @@ $_SESSION['sidebar_admin'] = "dashboard";
                 </div>
             </div>
             <!--Table -->
+            <!-- Note -->
+            <div class="container">
+                <div class="notes">
+                    <div id="create">
+
+                    </div>
+                    <div class="note">
+                        
+                    </div>
+                </div>
+            </div>
+            <!-- Note -->
         </div>
-        </div>
-        </div>
-        </div>
+        
 
 
 
