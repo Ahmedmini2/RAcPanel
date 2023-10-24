3563fb2b9fb17ce2d023b1e5683db903e41a5dfe
mohammed jamal <yamni.zero@gmail.com>
2023-10-24 12:16:32 +0200

    csa

--- Admin/Employee/profile-employee.php
+++ Admin/Employee/profile-employee.php
@@ -221,141 +221,90 @@ $select =mysqli_query($conn, "select * from users");
     <div class="container-fluid py-4">
         
     <div class="row">
+                <div class="col-12">
+                    <div class="card mb-4 mt-3">
+                    
+                        <div class="card-body px-0 pt-0 pb-2 mx-3">
+                            <div class="table-responsive p-0">
+                                <table class="table table-hover table-fixed" id="example">
+
+                                    <!--Table head-->
+                                    <thead class="bg-dark text-light text-center">
+                                        <tr>
+                                            <th>الرقم</th>
+                                            <th>اسم الموظف</th>
+                                            <th>قسم الموظف</th>
+                                            <th>راتب الموظف</th>
+                                            <th>العقد</th>
+                                            <th>بداية العقد</th>
+                                            <th>عدد ساعات العمل</th>
+                                            
+                                            <th>شهر</th>
+                                            <th>ACTION</th>
+                                        </tr>
+                                    </thead>
+                                    <!--Table head-->
+                                    <!--Table body-->
+                                    <tbody class=" text-center">
+
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>عباس الجعفري</td>
+                                            <td>المدير العام</td>
+                                            <td>5000</td>
+                                            <td>العقد</td>
+                                            <td>1/10/2023</td>
+                                            <td>9</td>
+                                            
+                                            <td>اكتوبر 2023</td>
+                                            <td>
+                                                <a href="view-employee.php"><i class="fa fa-eye" aria-hidden="true"></i></a>| 
+                                                <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>عباس الجعفري</td>
+                                            <td>المدير العام</td>
+                                            <td>5000</td>
+                                            <td>العقد</td>
+                                            <td>1/10/2023</td>
+                                            <td>9</td>
+                                           
+                                            <td>اكتوبر 2023</td>
+                                            <td>
+                                                <a href="view-employee.php"><i class="fa fa-eye" aria-hidden="true"></i></a>| 
+                                                <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                            </td>
     
-        <div class="col-12 col-xl-4">
-          <div class="card h-100" dir="ltr">
-            <div class="card-header pb-0 p-3" dir="rtl">
-              <div class="row">
-                <div class="col-md-8 d-flex align-items-center">
-                  <h6 class="mb-0">معلومات شخصية</h6>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>عباس الجعفري</td>
+                                            <td>المدير العام</td>
+                                            <td>5000</td>
+                                            <td>العقد</td>
+                                            <td>1/10/2023</td>
+                                            <td>9</td>
+                                            
+                                            <td>اكتوبر 2023</td>
+                                            <td>
+                                                <a href="view-employee.php"><i class="fa fa-eye" aria-hidden="true"></i></a>| 
+                                                <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                            </td>
+    
+                                        </tr>
+
+
+                                    </tbody>
+                                    <!--Table body-->
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4 text-start">
-                  <a href="javascript:;">
-                    <i class="fas fa-user-edit text-secondary text-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Profile"></i>
-                  </a>
-                </div>
-              </div>
             </div>
-            <div class="card-body p-3">
-              <p class="text-sm">
-                Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).
-              </p>
-              <hr class="horizontal gray-light my-4">
-              <ul class="list-group">
-                <li class="list-group-item border-0 ps-0 pt-0 text-sm"><strong class="text-dark">Full Name:</strong> &nbsp; عباس عثمان الجعفري</li>
-                <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Mobile:</strong> &nbsp; (44) 123 1234 123</li>
-                <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Email:</strong> &nbsp; alecthompson@mail.com</li>
-                <li class="list-group-item border-0 ps-0 text-sm"><strong class="text-dark">Location:</strong> &nbsp; السعودية</li>
-                <li class="list-group-item border-0 ps-0 pb-0">
-                  <strong class="text-dark text-sm">Social:</strong> &nbsp;
-                  <a class="btn btn-facebook btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                    <i class="fab fa-facebook fa-lg"></i>
-                  </a>
-                  <a class="btn btn-twitter btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                    <i class="fab fa-twitter fa-lg"></i>
-                  </a>
-                  <a class="btn btn-instagram btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
-                    <i class="fab fa-instagram fa-lg"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-xl-4">
-          <div class="card h-100" dir="ltr">
-            <div class="card-header pb-0 p-3"dir="rtl">
-              <h6 class="mb-0">المحادثات</h6>
-            </div>
-            <div class="card-body p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                  <div class="avatar me-3">
-                    <img src="../../assets/img/kal-visuals-square.jpg" alt="kal" class="border-radius-lg shadow">
-                  </div>
-                  <div class="d-flex align-items-start flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Sophie B.</h6>
-                    <p class="mb-0 text-xs">Hi! I need more information..</p>
-                  </div>
-                  <a class="btn btn-link pe-3 ps-0 mb-0 ms-auto" href="javascript:;">Reply</a>
-                </li>
-                <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                  <div class="avatar me-3">
-                    <img src="../../assets/img/marie.jpg" alt="kal" class="border-radius-lg shadow">
-                  </div>
-                  <div class="d-flex align-items-start flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Anne Marie</h6>
-                    <p class="mb-0 text-xs">Awesome work, can you..</p>
-                  </div>
-                  <a class="btn btn-link pe-3 ps-0 mb-0 ms-auto" href="javascript:;">Reply</a>
-                </li>
-                <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                  <div class="avatar me-3">
-                    <img src="../../assets/img/ivana-square.jpg" alt="kal" class="border-radius-lg shadow">
-                  </div>
-                  <div class="d-flex align-items-start flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Ivanna</h6>
-                    <p class="mb-0 text-xs">About files I can..</p>
-                  </div>
-                  <a class="btn btn-link pe-3 ps-0 mb-0 ms-auto" href="javascript:;">Reply</a>
-                </li>
-                <li class="list-group-item border-0 d-flex align-items-center px-0 mb-2">
-                  <div class="avatar me-3">
-                    <img src="../../assets/img/team-4.jpg" alt="kal" class="border-radius-lg shadow">
-                  </div>
-                  <div class="d-flex align-items-start flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Peterson</h6>
-                    <p class="mb-0 text-xs">Have a great afternoon..</p>
-                  </div>
-                  <a class="btn btn-link pe-3 ps-0 mb-0 ms-auto" href="javascript:;">Reply</a>
-                </li>
-                <li class="list-group-item border-0 d-flex align-items-center px-0">
-                  <div class="avatar me-3">
-                    <img src="../../assets/img/team-3.jpg" alt="kal" class="border-radius-lg shadow">
-                  </div>
-                  <div class="d-flex align-items-start flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Nick Daniel</h6>
-                    <p class="mb-0 text-xs">Hi! I need more information..</p>
-                  </div>
-                  <a class="btn btn-link pe-3 ps-0 mb-0 ms-auto" href="javascript:;">Reply</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-      <footer class="footer pt-3  ">
-        <div class="container-fluid">
-          <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-              <div class="copyright text-center text-sm text-muted text-lg-end">
-                © <script>
-                  document.write(new Date().getFullYear())
-                </script>,
-                Made by
-                <a href="" class="font-weight-bold" target="_blank">Rukn Amial</a>
-                
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                <li class="nav-item">
-                  <a href="https://ruknamial.com" class="nav-link text-muted" target="_blank">Rukn Amial</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://files.ruknamial.com" class="nav-link text-muted" target="_blank">Files</a>
-                </li>
-                <li class="nav-item">
-                  <a href="https://ruknamial.com/blogs" class="nav-link text-muted" target="_blank">Blog</a>
-                </li>
-                
-              </ul>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </div>
   </main>
   
   <!--   Core JS Files   -->
