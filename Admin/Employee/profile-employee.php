9d4ff07b752bbdc70ee265e6a29266e0ed1cfc89
mohammed jamal <yamni.zero@gmail.com>
2023-10-25 09:42:14 +0200

    sca

--- Admin/Employee/profile-employee.php
+++ Admin/Employee/profile-employee.php
@@ -278,74 +278,76 @@ $select = mysqli_query($conn, "select * from users");
             </div>
           </div>
         </div>
-        <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="numbers">
-                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
-                                        <h5 class="font-weight-bolder mb-0">
-                                            5000
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div class="col-4 text-start">
-
-                                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                        <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="row">
+          <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
+            <div class="card">
+              <div class="card-body p-3">
+                <div class="row">
+                  <div class="col-8">
+                    <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
+                      <h5 class="font-weight-bolder mb-0">
+                        5000
+                      </h5>
                     </div>
+                  </div>
+                  <div class="col-4 text-start">
+
+                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                      <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="numbers">
-                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
-                                        <h5 class="font-weight-bolder mb-0">
-                                            5000
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div class="col-4 text-start">
-
-                                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                        <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div class="card">
+              <div class="card-body p-3">
+                <div class="row">
+                  <div class="col-8">
+                    <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
+                      <h5 class="font-weight-bolder mb-0">
+                        5000
+                      </h5>
                     </div>
+                  </div>
+                  <div class="col-4 text-start">
+
+                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                      <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="col-lg-3 col-sm-6 mb-lg-0 mb-4">
-                    <div class="card">
-                        <div class="card-body p-3">
-                            <div class="row">
-                                <div class="col-8">
-                                    <div class="numbers">
-                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
-                                        <h5 class="font-weight-bolder mb-0">
-                                            5000
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div class="col-4 text-start">
-
-                                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                                        <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div class="card">
+              <div class="card-body p-3">
+                <div class="row">
+                  <div class="col-8">
+                    <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">المرتب</p>
+                      <h5 class="font-weight-bolder mb-0">
+                        5000
+                      </h5>
                     </div>
-          </div>
+                  </div>
+                  <div class="col-4 text-start">
 
+                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                      <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <footer class="footer pt-3  ">
         <div class="container-fluid">
           <div class="row align-items-center justify-content-lg-between">
