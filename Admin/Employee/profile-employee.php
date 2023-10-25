bb31b4cd53d7b9bae7ae06b0082b3892492e2580
mohammed jamal <yamni.zero@gmail.com>
2023-10-25 10:04:04 +0200

    sac

--- Admin/Employee/profile-employee.php
+++ Admin/Employee/profile-employee.php
@@ -284,9 +284,31 @@ $select = mysqli_query($conn, "select * from users");
               </div>
             </div>
           </div>
+          <div class="sal-card col-lg-4 col-sm-6  mb-4">
+            <div class="sal-card-info">
+              <div class="card-body p-3">
+                <div class="row">
+                  <div class="col-8">
+                    <div class="numbers">
+                      <p class="text-sm mb-0 text-capitalize font-weight-bold">متبقي المرتب</p>
+                      <h5 class="font-weight-bolder mb-0">
+                        2500
+                      </h5>
+                    </div>
+                  </div>
+                  <div class="col-4 text-start">
+
+                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                      <i class="fa fa-hourglass text-lg opacity-10" aria-hidden="true"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      
+
 
       <footer class="footer pt-3  ">
         <div class="container-fluid">
