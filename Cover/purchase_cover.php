0cb57b51fac1003c39b8f5e1dc6688f5f2bbaf78
mohammed jamal <yamni.zero@gmail.com>
2023-10-25 11:33:53 +0200

    sac

--- Cover/purchase_cover.php
+++ Cover/purchase_cover.php
@@ -312,9 +312,9 @@ if (isset($_GET['id'])) {
                 <button type="button" id="btn3" class="printing printing2 btn bg-gradient-dark rounded-pill col-md-2 col-sm-6 col-xs-5  " data-bs-toggle="modal" data-bs-target="#exampleModal2">
                     إرفاق \ عرض الملف
                 </button>
+                <a href="review_orders.php" class="btn bg-gradient-dark justify-content-md-start mb-0 col-md-2 col-sm-6 col-xs-6"> مراجعه الطلبيات&nbsp;&nbsp; <i class="fas fa-plus"></i></a>
 
             </div>
-            <a href="review_orders.php" class="btn bg-gradient-dark mb-0 col-md-2 col-sm-6 col-xs-6"> مراجعه الطلبيات&nbsp;&nbsp; <i class="fas fa-plus"></i></a>
 
             <script>
                 function printDiv(divName) {
