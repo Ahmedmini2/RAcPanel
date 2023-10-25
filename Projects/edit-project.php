527a4d00bb494ddc0b982bc24639139bf5b2eb6d
Ahmedmini2 <liltemes@gmail.com>
2023-10-25 11:49:58 +0400

    يشص

--- Projects/edit-project.php
+++ Projects/edit-project.php
@@ -8,9 +8,7 @@ if(isset($_GET['project_id']) && isset($_GET['item_id'])){
     $query = "SELECT * FROM projects WHERE `id` = $project_id";
     $res = $conn->query($query);
     $project = $res->fetch_assoc();
-    $project_name = $project['name'];
-    $project_description = $project['description'];
-    $project_cost = $project['project_cost'];
+
     $total_without_tax = $project['total_without_tax'];
     $total_with_tax = $project['total_with_tax'];
     $net_total = $project['net_total'];
@@ -18,16 +16,6 @@ if(isset($_GET['project_id']) && isset($_GET['item_id'])){
     $duration = $project['duration'];
     $payment_type = $project['payment_type'];
 
-    $query2 = "SELECT * FROM contact_projects WHERE `project_id` = $project_id";
-    $res2 = $conn->query($query2);
-    $contact_project = $res2->fetch_assoc();
-    $supplier_name = $contact_project['supplier_name'];
-    $contact_person = $contact_project['contact_person'];
-    $mobile = $contact_project['mobile'];
-    $address = $contact_project['address'];
-    $email = $contact_project['email'];
-    $vat = $contact_project['vat'];
-    $company_trade = $contact_project['company_trade'];
 
     $query_pro = "SELECT * FROM products WHERE `project_id` = $project_id AND `id` = $item_id";
     $res_pro = $conn->query($query_pro);
@@ -57,29 +45,13 @@ if (isset($_POST['add-project'])) {
   $accessory1 = 1;
   $band1 = 1;
 
-  $project_name = $_POST['project_name'];
-  $project_description = $_POST['project_description'];
-  $duration = $_POST['duration'];
-  $payment_type = $_POST['payment_type'];
-  $valid_till = $_POST['valid_till'];
   $total_cost = $_POST['prod_peice_tot'];
   $total_cost = str_replace(',','',$total_cost);
   $total_net = $_POST['net_toti'];
   $total_without_tax = $total_cost + $total_net;
   $total_with_tax = ($total_without_tax * 15) /100; 
 
-  if(!empty(basename($_FILES["project_image"]["name"]))) {
-    $target_dir = "../Projects/Images/".$project_name."/";
-    if(!is_dir($target_dir)) {
-      mkdir($target_dir, 0777, true);
-    }else{
 
-    }
-    $target_file = $target_dir . basename($_FILES["project_image"]["name"]);
-    $filename = basename($_FILES["project_image"]["name"]);
-    $uploadOk = 1;
-    move_uploaded_file($_FILES["project_image"]["tmp_name"], $target_file);
-  }
   
 
 //   $insert_project = "INSERT INTO projects (`id`, `name`, `description`,`image`,`project_cost`,`total_without_tax`,`total_with_tax`,`net_total`,`valid_till`,`duration`,`payment_type`,`created_at`)
@@ -454,96 +426,6 @@ if (isset($_POST['add-project'])) {
             <h5 class="block-title text-white py-2 px-4 ">طلب تعديل مشروع</h5>
           </div>
           <form id="<?php echo $idAttr; ?>" action="#" method="post" enctype="multipart/form-data">
-            <h4>بيانات المشروع</h4>
-            <div class="row">
-              <div class="col-md-6 col-sm-6">
-                <div class="form-group">
-                  <label>أسم الجهة الطالبة للمشروع</label>
-                  <input type="text" placeholder="الرجاء كتابة أسم مشروع" class="form-control" name="project_name" value="<?=$project_name?>">
-                </div>
-              </div>
-              <div class="col-md-6 col-sm-6">
-                <div class="form-group">
-                  <label>صورة المشروع</label>
-                  <input type="file"  class="form-control" name="project_image">
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-12 col-sm-6">
-                <div class="form-group">
-                  <label> تفاصيل المشروع</label>
-                  <input type="text" placeholder="الرجاء كتابة تفاصيل" class="form-control" name="project_description" value="<?=$project_description?>">
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-4 col-sm-6">
-                <div class="form-group">
-                  <label> مدة تنفيذ المشروع</label>
-                  <input type="date" placeholder="" class="form-control" name="duration" value="<?=$duration?>">
-                </div>
-              </div>
-              <div class="col-md-4 col-sm-6">
-                <div class="form-group">
-                  <label> طريقة الدفع</label>
-                  <input type="text" placeholder="40 \ 60" class="form-control" name="payment_type" value="<?=$payment_type?>">
-                </div>
-              </div>
-              <div class="col-md-4 col-sm-6">
-                <div class="form-group">
-                  <label> فترة صلاحية التسعيرة</label>
-                  <input type="date" placeholder="" class="form-control" name="valid_till" value="<?=$valid_till?>">
-                </div>
-              </div>
-              <hr>
-            </div>
-            
-            <!-- Contact Detials -->
-            <div class="contact_details">
-              <h5>بيانات التواصل</h5>
-              <div class="contact">
-                <div class="row">
-                  <div class="col-md-2 col-sm-6">
-                    <div class="form-group">
-                      <label for="contact_name">الأسم</label>
-                      <input type="text" class="form-control" name='contact_name' id="contact_name" value="<?=$contact_person?>">
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-sm-6 ">
-                    <div class="form-group">
-                      <label for="mobile">رقم الهاتف</label>
-                      <input type="text" class="form-control" name='mobile' id="mobile" value="<?=$mobile?>">
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-sm-6 ">
-                    <div class="form-group">
-                      <label for="address">العنوان</label>
-                      <input type="text" class="form-control" name='address' id="address" value="<?=$address?>">
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-sm-6 ">
-                    <div class="form-group">
-                      <label for="email">البريد الإلكتروني</label>
-                      <input type="text" class="form-control" name='email' id="email" value="<?=$email?>">
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-sm-6 ">
-                    <div class="form-group">
-                      <label for="vat">الرقم الضريبي</label>
-                      <input type="text" class="form-control" name='vat' id="vat" value="<?=$vat?>">
-                    </div>
-                  </div>
-                  <div class="col-md-2 col-sm-6 ">
-                    <div class="form-group">
-                      <label for="trade">رقم السجل التجاري</label>
-                      <input type="text" class="form-control" name='trade' id="trade" value="<?=$company_trade?>">
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <hr>
-            </div>
             
            
             <!-- Product Details -->
