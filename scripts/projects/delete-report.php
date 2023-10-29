0975be2a5d577b641729322a5c0b6ccff1e38f25
Ahmedmini2 <liltemes@gmail.com>
2023-10-29 12:26:05 +0400

    daw

---
+++ scripts/projects/delete-report.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+$email_address= !empty($_SESSION['email'])?$_SESSION['email']:'';
+
+if(empty($email_address))
+{
+  header("location:../../Auth/sign-in.php");
+}
+// session_start();
+// if(empty($_SESSION['role'] || $_SESSION['role'] != '1')){
+    
+//     header('location:index');
+// }
+include '../../db/connection.php';
+
+    if(!empty($_GET['id']))
+                    {
+                        if(isset($_POST['del']))
+                        {
+                            $password = $_POST['pas'];
+                            $password=  md5($password);
+                            if($password == $_SESSION['password']){
+                            $id = $_GET['id'];
+                            $del= mysqli_query($conn, "delete from product_status where id = '$id'");
+                            if($del)
+                            {
+                                $_SESSION['notification'] = "تم حذف تقرير العمل بنجاح";
+                                header('location:../../Projects/index.php');
+                            }
+                            }else{
+                            $_SESSION['notification'] = "كلمة مرور خاطئة";
+                            header('location:../../Projects/index.php');
+                            }
+                        }
+                    }
+
+                    ?>
