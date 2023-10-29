c56c8749b96dbee54bf409c1cb52947b39ead9e9
mohammed jamal <yamni.zero@gmail.com>
2023-10-29 14:42:59 +0200

    asc

--- Profile/profle_setting.php
+++ Profile/profle_setting.php
@@ -214,107 +214,139 @@ if (!empty($_GET['edit'])) {
                 <h3>My Profile</h3>
                 <hr>
             </div>
-            <div class="row ">
-
-                <div class="col-12 col-xl-5">
-
-                    <!-- Form START  card h-100" dir="ltr-->
-                    <div class="row">
-                        <!-- Upload profile -->
-                        <div class="col-10">
-                            <h6 class="mb-4 mt-0">رفع صورة</h6>
-                            <div class="card">
-
-                                <div class="text-center">
-                                    <!-- Image upload -->
-                                    <div class="square position-relative display-2 mb-3">
-                                        <i class="fas fa-fw fa-user position-absolute top-50 start-50 translate-middle text-secondary"></i>
-                                    </div>
-                                    <!-- Button -->
-                                    <input type="file" id="customFile" name="file" hidden="">
-                                    <label class="btn btn-success-soft btn-block" for="customFile">Upload</label>
-                                    <button type="button" class="btn btn-danger-soft">Remove</button>
-                                    <!-- Content -->
-                                    <p class="text-muted mt-3 mb-0"><span class="me-1">Note:</span>Minimum size 300px x 300px</p>
-                                </div>
-                            </div>
-                        </div>
+            <form method="post">
 
 
+                <div class="row ">
 
+                    <div class="col-12 col-xl-5">
 
-                    </div>
-
-                </div>
-                <div class="col-12 col-xl-7">
-                    <h6 class="mb-4 mt-0">بيانات الموظف</h6>
-                    <div class="card p-2">
-                        <div class="row ">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label>First Name</label>
-                                    <input type="text" placeholder="First Name" class="form-control" name="EmployeeID" value="">
-
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="form-group">
-                                    <label>Last Name</label>
-                                    <input type="text" placeholder="Last Name" class="form-control" name="name" value="">
-                                </div>
-                            </div>
-                        </div>
+                        <!-- Form START  card h-100" dir="ltr-->
                         <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label>Mobile number</label>
-                                    <input type="text" placeholder="Mobile number" class="form-control" name="EmployeeID" value="">
+                            <!-- Upload profile -->
+                            <div class="col-10">
+                                <h6 class="mb-4 mt-0">رفع صورة</h6>
+                                <div class="card">
 
+                                    <div class="text-center">
+                                        <!-- Image upload -->
+                                        <div class="square position-relative display-2 mb-3">
+                                            <i class="fas fa-fw fa-user position-absolute top-50 start-50 translate-middle text-secondary"></i>
+                                            <div id="preview">
+
+                                            </div>
+                                        </div>
+                                        <!-- Button -->
+                                        <input type="file" id="customFile" name="file" hidden="" onchange="getImagePreview(event)">
+                                        <label class="btn btn-success-soft btn-block" for="customFile">Upload</label>
+                                        <button type="button" class="btn btn-danger-soft">Remove</button>
+                                        <!-- Content -->
+                                        <p class="text-muted mt-3 mb-0"><span class="me-1">Note:</span>Minimum size 300px x 300px</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col">
-                                <div class="form-group">
-                                    <label>Email</label>
-                                    <input type="text" placeholder="Email" class="form-control" name="name" value="">
+
+
+
+
+                        </div>
+
+                    </div>
+                    <div class="col-12 col-xl-7">
+                        <h6 class="mb-4 mt-0">بيانات الموظف</h6>
+                        <div class="card p-2">
+                            <div class="row ">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>First Name</label>
+                                        <input type="text" placeholder="First Name" class="form-control" name="EmployeeID" value="">
+
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Last Name</label>
+                                        <input type="text" placeholder="Last Name" class="form-control" name="name" value="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Mobile number</label>
+                                        <input type="text" placeholder="Mobile number" class="form-control" name="EmployeeID" value="">
+
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="text" placeholder="Email" class="form-control" name="name" value="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <h6 class="my-4">Change Password</h6>
+                                <div class="row">
+
+
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword1">Old password *</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1">
+
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword2">New password *</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword2">
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword3">Confirm Password *</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword3">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                </div>
-                <div class="row">
-                    <h6 class="my-4">Change Password</h6>
+                    </div>
                     <div class="row">
+                        <h6 class="my-4">Change Password</h6>
+                        <div class="row">
 
 
-                        <div class="col">
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Old password *</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Old password *</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword1">
 
+                                </div>
                             </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label for="exampleInputPassword2">New password *</label>
-                                <input type="password" class="form-control" id="exampleInputPassword2">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="exampleInputPassword2">New password *</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword2">
+                                </div>
                             </div>
-                        </div>
-                        <div class="col">
-                            <div class="form-group">
-                                <label for="exampleInputPassword3">Confirm Password *</label>
-                                <input type="password" class="form-control" id="exampleInputPassword3">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="exampleInputPassword3">Confirm Password *</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword3">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="gap-3 d-md-flex justify-content-md-end text-center">
-                <button type="button" class="btn btn-secondary ">Delete profile</button>
-                <button type="button" class="btn btn-secondary ">Update profile</button>
-            </div>
-
+                <div class="gap-3 d-md-flex justify-content-md-end text-center">
+                    <button type="button" class="btn btn-secondary ">Delete profile</button>
+                    <button type="button" class="btn btn-secondary ">Update profile</button>
+                </div>
+            </form>
         </div>
 
 
@@ -413,6 +445,12 @@ if (!empty($_GET['edit'])) {
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
     <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
     <script src="../Admin/darkmode.js"></script>
+    <script type="text/javascript">
+        function getImagePreview(event)
+        {
+            console.log(event.target.file[0]);
+        }
+    </script>
 </body>
 
 </html>
