c99bf9d1a6637ba1c2b6e53ed30edc5ba32fa6ca
Ahmedmini2 <liltemes@gmail.com>
2023-10-29 11:46:27 +0400

    dwa

--- Auth/sign-in.php
+++ Auth/sign-in.php
@@ -36,7 +36,7 @@ include '../scripts/login.php';
 </head>
 
 <body class="">
-  <div class="container position-sticky z-index-sticky top-0">
+  <div class="container-fluid position-sticky z-index-sticky top-0">
     <div class="row">
       <div class="col-12">
         <!-- Navbar -->
