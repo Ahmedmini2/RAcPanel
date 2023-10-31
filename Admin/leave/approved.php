de6d3147a277b6724fbd6c2b252e461ee037a876
mohammed jamal <yamni.zero@gmail.com>
2023-10-31 10:31:31 +0200

    a

--- Admin/leave/approved.php
+++ Admin/leave/approved.php
@@ -1,6 +1,8 @@
 <?php
 include('../../cookies/session3.php');
 $_SESSION['sidebar_admin'] = "leave";
+$select = mysqli_query($conn, "select * from leaves WHERE status = 'Approved'");
+
 ?>
 
 <html lang="ar" dir="rtl">
@@ -136,28 +138,38 @@ $_SESSION['sidebar_admin'] = "leave";
                                     <thead class="bg-dark text-light text-center">
                                         <tr>
                                             <th>الرقم</th>
-                                            <th>رقم الموظف</th>
+                                           
                                             <th>اسم الموظف</th>
                                             <th>leave type</th>
-                                            <th>تاريخ الطلب</th>
-                                            <th>حاله الطلب </th>
+                                            <th>description</th>
+                                            <th>تاريخ بداية الإجازة</th>
+                                            <th>تاريخ نهاية الإجازة</th>
+                                            <th>حاله الاجازة </th>
+                                            <th>Action</th>
                                             
                                         </tr>
                                     </thead>
                                     <!--Table head-->
                                     <!--Table body-->
                                     <tbody class=" text-center">
-
+                                    <?php 
+                                    $i = 0;
+                                    while ($r = mysqli_fetch_array($select)) {
+                                        $i++;
+                                        ?>
                                         <tr>
-                                            <th scope="row">1</th>
-                                            <td class="border-1" >RUKNAMIL0002</td>
-                                            <td class="border-1">عباس الجعفري</td>
-                                            <td class="border-1">عطلة</td>
-                                            <td class="border-1">2023/10/22</td>
-                                            <td class="border-1"><span class="badge badge-sm bg-gradient-success">Approved</span></td>
-                                            
+                                            <th scope="row"><?=$i?></th>
+                                            <td class="border-1"><?=$r['name']?></td>
+                                            <td class="border-1"><?=$r['type']?></td>
+                                            <td class="border-1"><?=$r['description']?></td>
+                                            <td class="border-1"><?=$r['start_date']?></td>
+                                            <td class="border-1"><?=$r['end_date']?></td>
+                                            <td class="border-1"><span class="badge badge-sm bg-gradient-success"><?=$r['status']?></span></td>
+                                            <td class="border-1">
+                                                <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                            </td>
                                         </tr>
-
+                                        <?php } ?>
 
                                     </tbody>
                                     <!--Table body-->
