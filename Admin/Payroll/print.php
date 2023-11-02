c1417c0676c6028e4749b6ece69ebe8bb71b2bcb
mohammed jamal <yamni.zero@gmail.com>
2023-11-02 14:15:17 +0200

    asc

--- Admin/Payroll/print.php
+++ Admin/Payroll/print.php
@@ -318,7 +318,7 @@ include('../../cookies/insert-method.php');
 
 
 
-                < <table>
+                <table>
                     <div class="row p-5 text-center">
                         <div class="col-4">
                             <div class="row">
@@ -350,41 +350,6 @@ include('../../cookies/insert-method.php');
 
 
         </div>
-
-
-
-
-
-        <footer class="footer pt-3  ">
-            <div class="container-fluid">
-                <div class="row align-items-center justify-content-lg-between">
-                    <div class="col-lg-6 mb-lg-0 mb-4">
-                        <div class="copyright text-center text-sm text-muted text-lg-end">
-                            © <script>
-                                document.write(new Date().getFullYear())
-                            </script>,
-                            made with <i class="fa fa-heart"></i> by
-                            <a href="" class="font-weight-bold" target="_blank">Rukn Amial</a>
-
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                            <li class="nav-item">
-                                <a href="https://ruknamial.com" class="nav-link text-muted" target="_blank">Rukn Amial</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://files.ruknamial.com" class="nav-link text-muted" target="_blank">Files</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="https://ruknamial.com/blogs" class="nav-link text-muted" target="_blank">Blog</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
         </div>
 
     </main>
