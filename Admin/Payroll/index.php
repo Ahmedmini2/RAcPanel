9b5937cb29924fda20eb6c9a6e3b085dbf4bf4a1
mohammed jamal <yamni.zero@gmail.com>
2023-11-02 10:59:55 +0200

    asc

--- Admin/Payroll/index.php
+++ Admin/Payroll/index.php
@@ -46,6 +46,7 @@ $select = mysqli_query($conn, "select * from payroll_process ");
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+    <script>table2excel.js</script>
 </head>
 
 
