0a25dff02b8c5c7404b50d250b07b967e52afb81
mohammed jamal <yamni.zero@gmail.com>
2023-11-02 13:52:40 +0200

    ascascasc

--- Admin/Payroll/print.php
+++ Admin/Payroll/print.php
@@ -47,7 +47,127 @@ include('../../cookies/insert-method.php');
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
+
+    <style>
+        .invoice-box {
+            max-width: 1200px;
+            margin: auto;
+            padding: 30px;
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
+        }
+
+        .row1 {
+            padding-top: 10px !important;
+            position: relative;
+            font-size: small;
+            text-align: right !important;
+        }
+
+        .row2 {
+            text-align: right !important;
+            font-size: small;
+        }
+
+        .invoice-box table {
+            width: 100%;
+            line-height: inherit;
+
+        }
+
+
+        .invoice-box table td {
+            padding: 5px;
+            vertical-align: top;
+            font-size: small;
+        }
+
+        .invoice-box table tr th {
+            color: black;
+        }
+
+        .invoice-box table tr td:nth-child(2) {
+            text-align: right;
+        }
+
+        .invoice-box table tr.top table td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+            text-align: center;
+        }
+
+        .invoice-box table tr.information table td {
+            padding-bottom: 40px;
+        }
+
+        .invoice-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+        }
+
+        .invoice-box table tr.details td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.item td {
+            border-bottom: 1px solid #eee;
+        }
+
+        .invoice-box table tr.item.last td {
+            border-bottom: none;
+        }
+
+        .invoice-box table tr.total td:nth-child(2) {
+            border-top: 2px solid #eee;
+            font-weight: bold;
+        }
+
+
+
+
+        @media only screen and (max-width: 600px) {
+            .invoice-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+
+            .invoice-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
+
+        @page {
+            size: auto;
+            margin: 0mm;
+        }
+
+
+
+
+        @page {
+            size: auto;
+            margin: 0mm;
+        }
+
+        .printing2 {
+            left: 140px;
+        }
+    </style>
 </head>
+
+
+
 
 
 <body class="g-sidenav-show rtl  bg-gray-100">
