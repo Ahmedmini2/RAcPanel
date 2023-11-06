e824e17cb43dda9e0df41d317e92309056efa75a
Ahmedmini2 <liltemes@gmail.com>
2023-11-06 14:18:06 +0400

    dwa

--- index.php
+++ index.php
@@ -728,8 +728,8 @@ while ($ban = mysqli_fetch_array($banner)) {
       // Loop through the received notifications and add them to the UI
       data.forEach(function(notification) {
         const notificationItem = $('<li>').addClass('mb-2 r').attr('dir', 'ltr');
-        const notificationLink = $('<a>').addClass('dropdown-item border-radius-md').attr('href', 'javascript:;').html('<span class="ribbon-pop">NEW</span>');
-        const notificationMainDiv = $('<div>').addClass('d-flex py-1');
+        const notificationLink = $('<a>').addClass('dropdown-item border-radius-md').attr('href', 'javascript:;');
+        const notificationMainDiv = $('<div>').addClass('d-flex py-1').html('<span class="ribbon-pop">NEW</span>');
         const notificationImgDiv = $('<div>').addClass('my-auto');
         const notificationImg = $('<img>').addClass('avatar avatar-sm  me-3 ').attr('src','assets/img/team-2.jpg');
         const notificationMsgDiv = $('<div>').addClass('d-flex flex-column justify-content-center');
