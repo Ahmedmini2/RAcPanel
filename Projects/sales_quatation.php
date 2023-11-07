b0c022bfe5c8ca0398d3b25fb289baebc7b66fda
mohammed jamal <yamni.zero@gmail.com>
2023-11-07 11:25:22 +0200

    asc

--- Projects/sales_quatation.php
+++ Projects/sales_quatation.php
@@ -423,7 +423,7 @@ if (isset($_GET['project_id'])) {
                                     ?>
 
                                         <tr>
-                                            <th class="table-secondary text-center bor border-1" scope="row"><?= $i ?></th>
+                                            <th class=" text-center bor border-1" scope="row"><?= $i ?></th>
                                             <td class="custom-font-m text-center border-1"><?= $item['product_name'] ?></td>
                                             <td class="custom-font-m border-1"><?= $item['quantity'] ?></td>
                                             <td class="custom-font-m border-1"><?= number_format($item['sell_price']) ?></td>
