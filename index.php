5995dfbe8cce685144a3615c9900733161ab0cd7
mohammed jamal <yamni.zero@gmail.com>
2023-11-08 15:17:15 +0200

    سؤش

--- index.php
+++ index.php
@@ -73,59 +73,10 @@ while ($ban = mysqli_fetch_array($banner)) {
   <!-- End Of side Bar -->
   <main class="main-content position-relative lg:max-height-vh-100 lg:h-100 mt-1 border-radius-lg overflow-hidden" style="-webkit-overflow-scrolling: touch;overflow-y: scroll;">
     <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
-      <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 ">
-            <li class="breadcrumb-item text-sm ps-2"><a class="opacity-5 text-dark" href="javascript:;">لوحات القيادة</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">الرئيسية</li>
-          </ol>
-          <h6 class="font-weight-bolder mb-0">الرئيسية</h6>
-        </nav>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 px-0" id="navbar">
-        
-          
-          <ul class="navbar-nav me-auto ms-0 justify-content-end">
-            <li class="nav-item d-flex align-items-center px-4">
-              <a href="../Auth/logout.php" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none"> تسجيل الخروج</span>
-              </a>
-            </li>
-            <li class="nav-item d-xl-none pe-3 d-flex align-items-center">
-              <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
-                <div class="sidenav-toggler-inner">
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                  <i class="sidenav-toggler-line"></i>
-                </div>
-              </a>
-            </li>
-            <li class="nav-item px-3 d-flex align-items-center">
-              <a href="Messages/chat.php" class="nav-link text-body p-0">
-
-                <i class="far fa-comments me-sm-1 cursor-pointer"></i>
-              </a>
-            </li>
-
-            <!-- Notifications -->
-            <li class="nav-item dropdown ps-2 d-flex align-items-center px-4">
-              <a href="javascript:;" class="nav-link text-body p-0" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fa fa-bell cursor-pointer"></i>
-                <span id="notification-count" class="notification-badge">0</span> 
-              </a>
-              <ul class="dropdown-menu  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton" id="notifications-container">
-               
-              </ul>
-            </li>
-            <!-- End of Notifications -->
-
-            
-
-          </ul>
-        </div>
-      </div>
-    </nav>
+     <?php 
+     $titleNav = 'الرئيسية';
+     require_once('components/navbar.php');
+     ?>
     <!-- End Navbar -->
 
     <?php if($position != 'Admin'){?>
