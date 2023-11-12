16c161989be726aee4f0bc5404a071cb4c49235a
Ahmedmini2 <liltemes@gmail.com>
2023-11-12 15:36:17 +0400

    da

--- Projects/purchase_order_two.php
+++ Projects/purchase_order_two.php
@@ -564,7 +564,7 @@ if (isset($_GET['project_id'])) {
                                     </td>
                                     <td>
                                         <div class="text-right">
-                                            <span class="font-weight-bold text-success" id="total"><?=number_format($total_without_tax+$total_with_tax+$del_total_price)?></span>
+                                            <span class="font-weight-bold text-success" id="total"><?=number_format($total_without_tax+$total_with_tax)?></span>
                                         </div>
                                     </td>
                                 </tr>
@@ -581,7 +581,7 @@ if (isset($_GET['project_id'])) {
 
                 <div class="row">
                     <div class="col text-center">
-                        <p class="text-dark">The total value is SAR <?=number_format($total_without_tax+$total_with_tax+$del_total_price)?> <span id="con"></span> riyals only.</p>
+                        <p class="text-dark">The total value is SAR <?=number_format($total_without_tax+$total_with_tax)?> <span id="con"></span> riyals only.</p>
                         <p class="text-dark">The Price including Delivery Fees.</p>
                     </div>
                 </div>
@@ -589,7 +589,7 @@ if (isset($_GET['project_id'])) {
                     
                      function changeVal() {
                         
-                        value =  <?=number_format($total_without_tax+$total_with_tax+$del_total_price,0,"","")?> ;
+                        value =  <?=number_format($total_without_tax+$total_with_tax,0,"","")?> ;
                         document.getElementById("con").innerText = numToWords(value);
                         console.log(value);
                         
