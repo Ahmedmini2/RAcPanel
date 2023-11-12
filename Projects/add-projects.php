70df224053041f470fbb27fcfd693910863923f3
mohammed jamal <yamni.zero@gmail.com>
2023-11-12 11:15:16 +0200

    asc

--- Projects/add-projects.php
+++ Projects/add-projects.php
@@ -695,8 +695,8 @@ if (isset($_POST['add-project'])) {
                   <hr>
                   <h5>بند الحديد</h5>
                   <div class="col-md-2 col-sm-6 align-self-center flex-fill">
-                        
-                  </div>
+                        <span class="pull-right clickable close-icon text-start" data-effect="fadeOut"><i class="fa fa-times"></i></span>
+                        </div>
                   <div class="iron" id="main-iron">
                     <div class="row" id="row<?= $coco ?>">
                       <div class="col-md-2 col-sm-6">
