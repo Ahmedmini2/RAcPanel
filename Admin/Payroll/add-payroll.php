b01a672d63b9505c5227cf325126e5a0018a6736
mohammed jamal <yamni.zero@gmail.com>
2023-11-13 14:33:38 +0200

    bb

--- Admin/Payroll/add-payroll.php
+++ Admin/Payroll/add-payroll.php
@@ -216,7 +216,7 @@ if (isset($_POST['submit'])) {
                 type: "POST",
                 data: 'request=' + value,
                 beforeSend: function() {
-                    $(".table-responsive").html("<div class='loaderSelect text-center'><span>Loading....</span></div>");
+                    $(".table-responsive").html("<div class='loaderSelect'><span>Loading....</span></div>");
                 },
                 success: function(data) {
                     $(".table-responsive").html(data);
