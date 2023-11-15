fd79981932329424b1d6fa30f4fabd213b46d00d
Ahmedmini2 <liltemes@gmail.com>
2023-11-15 14:19:28 +0400

    d

--- Cover/index.php
+++ Cover/index.php
@@ -91,23 +91,18 @@ $select = mysqli_query($conn, "select * from covers_purchase_id");
                 $select2 = mysqli_query($conn, "SELECT * FROM covers_purchase WHERE purchase_id = '$purchase_id'");
                 while ($row = mysqli_fetch_array($select2)){
                   $type += '<br>' + (string)$row['type'] + '<br>';
-                  $dimensions += '<br>' .(string)$row['dimensions'].'<br>';
-                  $quantity += '<br>' .(string)$row['quantity'].'<br>';
-                  $price_per_piece += '<br>' .(string)$row['price_per_piece'].'<br>';
-                  $total_price += '<br>' .(string)$row['total_price'].'<br>';
-                  $created_at += '<br>' .(string)$row['created_at'].'<br>';
-                  $cover_id = $r['id'];
+                  
                 }
                 ?>
                     <tr class="text-center">
 
                       <td class="text-xs text-secondary mb-0 border-1"><?=$i?></td>
                       <td class="text-xs text-secondary mb-0 border-1"><?=$type?></td>
-                      <td class="mb-0 text-sm text-secondary border-1"><?=$dimensions?></td>
+                      <!-- <td class="mb-0 text-sm text-secondary border-1"><?=$dimensions?></td>
                       <td class="mb-0 text-sm text-secondary border-1"><?=$quantity?></td>
                       <td class="mb-0 text-sm text-secondary border-1"><?=$price_per_piece?></td>
                       <td class="mb-0 text-sm text-secondary  border-1"><?=$total_price?></td>
-                      <td class="text-xs text-secondary mb-0 border-1"><?=$created_at?></td>
+                      <td class="text-xs text-secondary mb-0 border-1"><?=$created_at?></td> -->
                       
 
                       <td class="border-1 text-secondary"><a href="purchase_cover.php?id=<?=$r['purchase_id']?>"><i class="fa fa-eye" aria-hidden="true"></i></a> <?php if ($position == 'Admin') { ?> |
