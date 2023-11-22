a62e6bc65d3384dd58152a2e63baef50291b3a6d
Ahmedmini2 <liltemes@gmail.com>
2023-11-22 14:44:48 +0400

    dwa

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ include('cookies/session.php');
 $_SESSION['sidebar'] = "Home";
 
 $show_products_status = mysqli_query($conn, "SELECT * FROM `product_status`");
-$projects = mysqli_query($conn, "SELECT * FROM projects WHERE status = 'قيد التنفيذ' LIMIT 3");
+$projects = mysqli_query($conn, "SELECT * FROM projects LIMIT 3");
 
-$banner = mysqli_query($conn, "SELECT * FROM projects LIMIT 3");
+$banner = mysqli_query($conn, "SELECT * FROM projects status = 'قيد التنفيذ'");
 while ($ban = mysqli_fetch_array($banner)) {
   $total_price += $ban['total_without_tax'];
   $total_cost += $ban['project_cost'];
