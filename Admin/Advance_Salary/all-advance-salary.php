9916d8c813e20d2aae1c34ba06a3c57b4a190b2c
Ahmedmini2 <liltemes@gmail.com>
2023-11-28 13:45:17 +0400

    dwa

--- Admin/Advance_Salary/all-advance-salary.php
+++ Admin/Advance_Salary/all-advance-salary.php
@@ -1,7 +1,7 @@
 <?php
 include('../../cookies/session3.php');
 $_SESSION['sidebar_admin'] = "Advance_Salary";
-$select = mysqli_query($conn, "SELECT DISTINCT `month`,`year` FROM `payroll_process` ORDER by `year` DESC");
+$select = mysqli_query($conn, "SELECT * FROM `employee`");
 ?>
 
 <html lang="ar" dir="rtl">
@@ -126,40 +126,10 @@ $select = mysqli_query($conn, "SELECT DISTINCT `month`,`year` FROM `payroll_proc
 
             <!--Table     -->
              <!--Table    -->
-             <?php
-            while ($r = mysqli_fetch_array($select)) {
-                $month = $r['month'];
-                $year = $r['year'];
-                $nmonth = date("m", strtotime($month));
-                $select2 = mysqli_query($conn, "select * FROM payroll_process WHERE month = '$month' AND year = '$year'");
-                
-                
-
-            ?>
+            
                 <div class="row">
                     <div class="col-12">
                         <div class="card mb-4 mt-3">
-                            <div class="card-header text-center">
-                                <div class="row align-items-center">
-                                    <div class="col-md-11">
-                                        <h5>مسير سلفيات مدد لشهر <?= $r['month'] ?> (<?= $nmonth ?>) -السنة الميلادية (<?= $year ?>)</h5>
-                                    </div>
-                                    <a href="print.php?month=<?= $month ?>&year=<?= $year ?>" class="printer">
-                                        <div class="paper">
-                                            <svg viewBox="0 0 8 8" class="svg">
-                                                <path fill="#0077FF" d="M6.28951 1.3867C6.91292 0.809799 7.00842 0 7.00842 0C7.00842 0 6.45246 0.602112 5.54326 0.602112C4.82505 0.602112 4.27655 0.596787 4.07703 0.595012L3.99644 0.594302C1.94904 0.594302 0.290039 2.25224 0.290039 4.29715C0.290039 6.34206 1.94975 8 3.99644 8C6.04312 8 7.70284 6.34206 7.70284 4.29715C7.70347 3.73662 7.57647 3.18331 7.33147 2.67916C7.08647 2.17502 6.7299 1.73327 6.2888 1.38741L6.28951 1.3867ZM3.99679 6.532C2.76133 6.532 1.75875 5.53084 1.75875 4.29609C1.75875 3.06133 2.76097 2.06018 3.99679 2.06018C4.06423 2.06014 4.13163 2.06311 4.1988 2.06905L4.2414 2.07367C4.25028 2.07438 4.26057 2.0758 4.27406 2.07651C4.81533 2.1436 5.31342 2.40616 5.67465 2.81479C6.03589 3.22342 6.23536 3.74997 6.23554 4.29538C6.23554 5.53084 5.23439 6.532 3.9975 6.532H3.99679Z"></path>
-                                                <path fill="#0055BB" d="M6.756 1.82386C6.19293 2.09 5.58359 2.24445 4.96173 2.27864C4.74513 2.17453 4.51296 2.10653 4.27441 2.07734C4.4718 2.09225 5.16906 2.07947 5.90892 1.66374C6.04642 1.58672 6.1743 1.49364 6.28986 1.38647C6.45751 1.51849 6.61346 1.6647 6.756 1.8235V1.82386Z"></path>
-                                            </svg>
-                                        </div>
-                                        <div class="dot"></div>
-                                        <div class="output">
-                                            <div class="paper-out"></div>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-
-                           
 
                             <div class="card-body px-0 pt-0 pb-2 mx-3">
                                 <div class="table-responsive p-0">
@@ -182,14 +152,19 @@ $select = mysqli_query($conn, "SELECT DISTINCT `month`,`year` FROM `payroll_proc
                                         <!--Table head-->
                                         <!--Table body-->
                                         <?php
-                                        while ($rr = mysqli_fetch_array($select2)) {
-                                             $i++;
-                                            $emp_id = $r['employee_id'];
-                                            $query = "SELECT * FROM users WHERE id=$emp_id";
+                                        while ($rr = mysqli_fetch_array($select)) {
+                                             
+                                            $emp_id = $rr['user_id'];
+                                            $query = "SELECT SUM(`amount`) as amount FROM `advance_salary` WHERE `employee_id` = $emp_id";
                                             $res = $conn->query($query);
-                                            $total_adv += $rr['advanced'];
-                                            $total_paid += $rr['advanced'];
-                                            $total_rem =  $total_adv - $total_paid
+                                            $advanc_data = $res->fetch_assoc();
+                                            $total_adv = $advanc_data['amount'];
+
+                                            $query = "SELECT * FROM `advance_status` WHERE `employee_id` = $emp_id";
+                                            $res2 = $conn->query($query2);
+                                            $status_data = $res2->fetch_assoc();
+                                            $total_status = $status_data['amount'];
+                                            
                                            
                                         ?>
                                             <tbody class=" text-center">
@@ -200,7 +175,7 @@ $select = mysqli_query($conn, "SELECT DISTINCT `month`,`year` FROM `payroll_proc
                                                     <td class="border-1"><?= $rr['salary'] ?></td>
                                                     <td class="border-1"><?= $total_adv ?></td>
                                                     <td class="border-1"><?= $total_paid ?></td>
-                                                    <td class="border-1"><?=  $total_rem ?></td>
+                                                    <td class="border-1"><?=   $total_status ?></td>
                                                     <td class="border-1">
                                                     <a href="#"><i class="fa fa-eye" aria-hidden="true"></i></a>|
                                                     <a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a>
@@ -223,7 +198,7 @@ $select = mysqli_query($conn, "SELECT DISTINCT `month`,`year` FROM `payroll_proc
                     </div>
                 </div>
                 <!--Table -->
-            <?php } ?>
+            
             <!--Table -->
         </div>
 
