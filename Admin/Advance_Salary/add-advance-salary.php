a4b7c1e69d2ca582dd70dc19b51fab5684d3ec99
Ahmedmini2 <liltemes@gmail.com>
2023-11-28 14:32:09 +0400

    dwa

--- Admin/Advance_Salary/add-advance-salary.php
+++ Admin/Advance_Salary/add-advance-salary.php
@@ -39,7 +39,7 @@ if (isset($_POST['submit'])) {
         $insertData3 = insert_data($advance_status_data, $tableName2);
         if ($insertData3) {
             $_SESSION['notification'] = "اضافة سلفية جديدة";
-            echo "صح";
+            echo $last_amount;
         } else {
 
         }
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
             $insertData2 = update_advance_status_data($advance_status_data, $tableName2, $employee_id);
             if ($insertData2) {
                 $_SESSION['notification'] = "نفس الشي";
-                echo "نفس الشي";
+                echo $last_amount;
             } else {
             }
         }
