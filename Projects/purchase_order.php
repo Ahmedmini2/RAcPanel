80b8ec0c03d17ec1e1a6725c2846e6f54789da23
mohammed jamal <yamni.zero@gmail.com>
2024-07-14 11:46:23 +0200

    -added selected in purchase

--- Projects/purchase_order.php
+++ Projects/purchase_order.php
@@ -571,6 +571,12 @@ if (isset($_GET['project_id'])) {
                         <p><?=$payment_type?>.</p>
                         <input type="text" class="signture signture2" id="signture3"/>
                     </div>
+                    <select class="form-select" aria-label="Default select example">
+                     <option selected>Open this select menu</option>
+                     <option value="1">One</option>
+                     <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
                 </div>
 
                 
