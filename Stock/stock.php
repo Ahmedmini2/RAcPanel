d0284d7f514cd4b98d78df06df792e5683e6796a
mohammed jamal <yamni.zero@gmail.com>
2024-08-05 12:33:48 +0200

    sa

--- Stock/stock.php
+++ Stock/stock.php
@@ -138,14 +138,10 @@ $select = mysqli_query($conn, "select * from stock");
 
                   <?php
                   while ($r = mysqli_fetch_array($select)) {
-                    $current_remaining_quantity = $r['quantity'];
-                    // حساب الكمية المتبقية بعد السحب
-                  $new_remaining_quantity = $current_remaining_quantity - $r['used_quantity'];
-                   
-                  
-                    $update = "UPDATE stock SET stock = '$new_remaining_quantity' WHERE id = $id";
-                    $updateResult = $conn->query($update);
+                    // $remaining_quantity = $r['quantity'] - $r['used_quantity'];
                     
+                    $stock = $r['quantity'] - $r['used_quantity'];
+                    $new_stock = $stock 
                   ?>
 
                     <tr class="text-center">
@@ -155,7 +151,7 @@ $select = mysqli_query($conn, "select * from stock");
                       <td data-label="الوصف" class="mb-0 text-sm text-secondary border-1"><?= $r['description'] ?></td>
                       <td data-label="الكمية" class="mb-0 text-sm text-secondary border-1"><?= $r['quantity'] ?></td>
                       <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" data-id="<?= $r['id'] ?>" value="<?= $r['used_quantity'] ?>" /></td>
-                      <td data-label="الكيمة المتبقيه" class="mb-0 text-sm text-secondary border-1"><?= $updateResult ?></td>
+                      <td data-label="الكيمة المتبقيه" class="mb-0 text-sm text-secondary border-1"><?= $new_stock ?></td>
                       <td data-label="سعر الصنف الواحد" class="mb-0 text-sm text-secondary border-1"><?= $r['price_per_piece'] ?></td>
                       <td data-label="الاجمالي" class="mb-0 text-sm text-secondary border-1"><?= $r['total_price'] ?></td>
                       <td data-label="صورة الفاتورة" class="mb-0 text-sm text-secondary border-1">
