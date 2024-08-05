ee3154ce82c445f75ceb6704f08b93a821b21e92
mohammed jamal <yamni.zero@gmail.com>
2024-08-06 01:13:36 +0200

    af

--- Stock/stock.php
+++ Stock/stock.php
@@ -138,9 +138,10 @@ $select = mysqli_query($conn, "select * from stock");
 
                   <?php
                   while ($r = mysqli_fetch_array($select)) {
-                    
-                    $remaining_quantity = $r['quantity'] - $r['used_quantity'];
-                    
+                    $nq = $_POST['nq'];
+                    $remaining_quantity = $r['quantity'] - $nq;
+                    // $remaining_quantity = $r['quantity'] - $r['used_quantity'];
+                  
                     $idS =  $r['id'];
                     $update = "UPDATE `stock` SET `stock` = '$remaining_quantity' WHERE `id` = $idS";
                     $updateResult = $conn->query($update);
@@ -156,7 +157,8 @@ $select = mysqli_query($conn, "select * from stock");
                       <td data-label="اسم المنج" class="text-xs text-secondary mb-0 border-1"><?= $r['name_stock'] ?></td>
                       <td data-label="الوصف" class="mb-0 text-sm text-secondary border-1"><?= $r['description'] ?></td>
                       <td data-label="الكمية" class="mb-0 text-sm text-secondary border-1"><?= $r['quantity'] ?></td>
-                      <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" data-id="<?= $r['id'] ?>" value="<?= $r['used_quantity'] ?>" /></td>
+                      <!-- <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" data-id="<?= $r['id'] ?>" value="<?= $r['used_quantity'] ?>" /></td> -->
+                      <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" value="nq" id="nq" name="nq"/></td>
                       <td data-label="الكيمة المتبقيه" class="mb-0 text-sm text-secondary border-1"><?= $new_stock ?></td>
                       <td data-label="سعر الصنف الواحد" class="mb-0 text-sm text-secondary border-1"><?= $r['price_per_piece'] ?></td>
                       <td data-label="الاجمالي" class="mb-0 text-sm text-secondary border-1"><?= $r['total_price'] ?></td>
