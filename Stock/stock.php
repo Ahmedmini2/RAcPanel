214cad744e686613cdcf2588c528479e55776cf8
mohammed jamal <yamni.zero@gmail.com>
2024-08-05 18:44:33 +0200

    asc

--- Stock/stock.php
+++ Stock/stock.php
@@ -144,7 +144,7 @@ $select = mysqli_query($conn, "select * from stock");
                     $idS =  $r['id'];
                     $update = "UPDATE `stock` SET `stock` = '$remaining_quantity' WHERE `id` = $idS";
                     $updateResult = $conn->query($update);
-                    $new_stock = $r['stock'] ;
+                    $new_stock = $r['stock'] - $r['used_quantity'];;
 
                   
                     
@@ -156,7 +156,6 @@ $select = mysqli_query($conn, "select * from stock");
                       <td data-label="اسم المنج" class="text-xs text-secondary mb-0 border-1"><?= $r['name_stock'] ?></td>
                       <td data-label="الوصف" class="mb-0 text-sm text-secondary border-1"><?= $r['description'] ?></td>
                       <td data-label="الكمية" class="mb-0 text-sm text-secondary border-1"><?= $r['quantity'] ?></td>
-                      <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" data-id="<?= $r['id'] ?>" value="<?= $r['used_quantity'] ?>" /></td>
                       <td data-label="الكمية المستهلكة" class="mb-0 text-sm text-secondary border-1"><input type="number" class="used-quantity-input" data-id="<?= $r['id'] ?>" value="<?= $r['used_quantity'] ?>" /></td>
                       <td data-label="الكيمة المتبقيه" class="mb-0 text-sm text-secondary border-1"><?= $new_stock ?></td>
                       <td data-label="سعر الصنف الواحد" class="mb-0 text-sm text-secondary border-1"><?= $r['price_per_piece'] ?></td>
